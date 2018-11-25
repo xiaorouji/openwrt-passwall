d062f05eb4eecabfc6887884d3a6b1542d6b8510
Lienol <lienol@qq.com>
2018-11-26 01:11:48 +0800

    update passwall

---
+++ lienol/luci-app-passwall/luasrc/model/cbi/passwall/auto_switch.lua
@@ -0,0 +1,39 @@
+local cursor=luci.model.uci.cursor()
+local i="passwall"
+
+local n={}
+cursor:foreach(i,"servers",function(e)
+	local server_type
+	if e.server_type == "ssr" then server_type = "SSR"
+	elseif e.server_type == "ss" then server_type = "SS"
+	elseif e.server_type == "v2ray" then server_type = "V2ray"
+	elseif e.server_type == "brook" then server_type = "Brook"
+	end
+	if e.server_type and e.server and e.remarks then
+		n[e[".name"]]="%s：[%s] %s"%{server_type,e.remarks,e.server}
+	end
+end)
+
+m=Map(i)
+s=m:section(TypedSection,"auto_switch",translate("Auto Switch"))
+s.anonymous = true
+
+o=s:option(Flag,"disconnect_reconnect_on",translate("Disconnection auto reconnection"),translate("Automatic switching cannot be used when this option is checked"))
+o.default=0
+o.rmempty=false
+
+o=s:option(Value,"disconnect_reconnect_time",translate("How often is a diagnosis made"),translate("Units:minutes"))
+o:depends("disconnect_reconnect_on",1)
+o.default="10"
+o.rmempty=true
+
+o=s:option(Value,"testing_time",translate("How often is a diagnosis made"),translate("Units:minutes"))
+o.default="10"
+o.rmempty=true
+o:depends("disconnect_reconnect_on",0)
+
+o=s:option(DynamicList,"tcp_redir_server",translate("List of alternate TCP forwarding servers"))
+for a,t in pairs(n)do o:value(a,t)end
+o:depends("disconnect_reconnect_on",0)
+
+return m
---
+++ lienol/luci-app-passwall/root/usr/share/passwall/auto_switch.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+Date=$(date "+%Y-%m-%d %H:%M:%S")
+TCP_REDIR_SERVER=`cat /var/etc/passwall_current_tcp_server`
+failcount=1
+while [ "$failcount" -lt "6" ]
+do
+	status=`curl -I -o /dev/null -s --connect-timeout 5 -w %{http_code} 'https://www.google.com.tw' |grep 200 `
+	if [ -z "$status" -o "$status" != "200" ];then
+		echo "$Date: 自动切换检测：第$failcount次检测异常" >> /var/log/passwall.log
+		let "failcount++"
+		[ "$failcount" -ge 6 ] && {
+			echo "$Date: 自动切换检测：检测异常，切换节点" >> /var/log/passwall.log
+			TCP_REDIR_SERVERS=`uci get passwall.@auto_switch[0].tcp_redir_server`
+			has_backup_server=`echo $TCP_REDIR_SERVERS | grep $TCP_REDIR_SERVER`
+			setserver=
+			if [ -z "$has_backup_server" ];then
+				setserver=`echo $TCP_REDIR_SERVERS | awk -F ' ' '{print $1}'`
+			else
+				flag=0
+				for server in $has_backup_server
+				do
+					if [ "$flag" == 0 ];then
+						if [ "$TCP_REDIR_SERVER" == "$server" ];then
+							flag=1
+							continue
+						fi
+					fi
+					if [ "$flag" == 1 ];then
+						flag=2
+						continue
+					fi
+					if [ "$flag" == 2 ];then
+						setserver=$server
+						break
+					fi
+				done
+			fi
+			uci set passwall.@global[0].tcp_redir_server=$setserver
+			uci commit passwall
+			/etc/init.d/passwall restart
+			exit 0
+		}
+		sleep 5s
+	else
+		echo "$Date: 自动切换检测：检测正常" >> /var/log/passwall.log
+		break
+	fi
+done
+exit
