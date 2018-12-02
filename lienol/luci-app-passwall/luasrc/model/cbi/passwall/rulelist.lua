f2359d5cec052ecb974712cfa1b65a7174fa043a
Lienol <lienol@qq.com>
2018-12-02 19:45:15 +0800

    update passwall
    Update to the latest rules
    Fix a small bug

--- lienol/luci-app-passwall/luasrc/model/cbi/passwall/rulelist.lua
+++ lienol/luci-app-passwall/luasrc/model/cbi/passwall/rulelist.lua
@@ -5,9 +5,9 @@ s.anonymous=true
 
 
 local t="/usr/share/passwall/rule/whitelist_host"
-o=s:option(TextValue,"whitelist",translate("Whitelist Hosts"))
+o=s:option(TextValue,"whitelist_host",translate("Whitelist Hosts"))
 o.description=translate("Join the white list of domain names will not go agent.")
-o.rows=8
+o.rows=5
 o.wrap="off"
 o.cfgvalue=function(a,a)
 	return fs.readfile(t)or""
@@ -23,9 +23,9 @@ end
 
 
 local t="/usr/share/passwall/rule/whitelist_ip"
-o=s:option(TextValue,"wiplist",translate("Whitelist IP/CIDR"))
+o=s:option(TextValue,"whitelist_ip",translate("Whitelist IP"))
 o.description=translate("These had been joined ip addresses will not use proxy.Please input the ip address or ip address segment,every line can input only one ip address.For example,112.123.134.145/24 or 112.123.134.145.")
-o.rows=8
+o.rows=5
 o.wrap="off"
 o.cfgvalue=function(a,a)
 	return fs.readfile(t)or""
@@ -36,9 +36,9 @@ end
 
 
 local t="/usr/share/passwall/rule/blacklist_host"
-o=s:option(TextValue,"weblist",translate("Blacklist Hosts"))
+o=s:option(TextValue,"blacklist_host",translate("Blacklist Hosts"))
 o.description=translate("These had been joined websites will use proxy.Please input the domain names of websites,every line can input only one website domain.For example,google.com.")
-o.rows=8
+o.rows=5
 o.wrap="off"
 o.cfgvalue=function(a,a)
 	return fs.readfile(t)or""
@@ -54,9 +54,9 @@ end
 
 
 local t="/usr/share/passwall/rule/blacklist_ip"
-o=s:option(TextValue,"iplist",translate("Blacklist IP/CIDR"))
+o=s:option(TextValue,"blacklist_ip",translate("Blacklist IP"))
 o.description=translate("These had been joined ip addresses will use proxy.Please input the ip address or ip address segment,every line can input only one ip address.For example,112.123.134.145/24 or 112.123.134.145.")
-o.rows=8
+o.rows=5
 o.wrap="off"
 o.cfgvalue=function(a,a)
 	return fs.readfile(t)or""
@@ -68,7 +68,7 @@ end
 local t="/usr/share/passwall/rule/router"
 o=s:option(TextValue,"routerlist",translate("router Hosts"))
 o.description=translate("These had been joined websites will use proxy,but only Router model.Please input the domain names of websites,every line can input only one website domain.For example,google.com.")
-o.rows=8
+o.rows=5
 o.wrap="off"
 o.cfgvalue=function(a,a)
 	return fs.readfile(t)or""
