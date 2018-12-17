05e4e956e47ce42d9f98ebbfecc860f6790ea673
Lienol <lawlienol@gmail.com>
2018-12-17 18:05:06 +0800

    update ha

--- lienol/luci-app-homeassistant/root/usr/share/homeassistant/download.sh
+++ lienol/luci-app-homeassistant/root/usr/share/homeassistant/download.sh
@@ -5,7 +5,10 @@ Date=$(date "+%Y-%m-%d %H:%M:%S")
 
 save_directory=`uci get $CONFIG.@global[0].save_directory`
 
-[ -z "$save_directory" ] && exit
+[ -z "$save_directory" ] && {
+	echo $Date: 未设置存放路径，停止下载... >> $LOG_FILE
+	exit
+}
 
 uci_get_by_type() {
 	local index=0
@@ -20,7 +23,7 @@ isinstallpy3=`opkg list-installed | grep python3`
 if [ -z "$isinstallpy3" ];then
 	echo $Date: 正在下载并安装Python3环境... >> $LOG_FILE
 	opkg update >> $LOG_FILE
-	opkg install python3 python3-pip python3-dev >> $LOG_FILE
+	opkg install python3 python3-pip python3-dev python3-cffi >> $LOG_FILE
 	echo $Date: 安装Python3环境完成... >> $LOG_FILE
 else
 	echo $Date: 已安装Python3环境，不需要再安装... >> $LOG_FILE
@@ -32,7 +35,7 @@ pip3 install setuptools >> $LOG_FILE
 pip3 install virtualenv >> $LOG_FILE
 virtualenv $save_directory -p /usr/bin/python3 >> $LOG_FILE
 source $save_directory/bin/activate >> $LOG_FILE
-pip3 install homeassistant==0.74.2 >> $LOG_FILE
-echo $Date: 进程运行完毕，启动软件！ >> $LOG_FILE
-/etc/init.d/$CONFIG restart
+#暂时不支持最新版HA，研究中。。。
+pip3 install homeassistant==0.75.2 >> $LOG_FILE
+echo $Date: 进程运行完毕！ >> $LOG_FILE
 exit
