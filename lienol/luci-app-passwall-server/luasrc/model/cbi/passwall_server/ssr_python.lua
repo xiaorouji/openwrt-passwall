d376fd7bff83d51284dec9e33d291e702605309b
Lienol <lawlienol@gmail.com>
2019-01-18 03:03:54 +0800

    passwall-server : big update !
    Powerful SSR Python luci interface center

--- lienol/luci-app-passwall-server/luasrc/model/cbi/passwall_server/ssr_python.lua
+++ lienol/luci-app-passwall-server/luasrc/model/cbi/passwall_server/ssr_python.lua
@@ -1,109 +1,143 @@
+local o = require "luci.dispatcher"
+local fs = require "nixio.fs"
+local sys = require "luci.sys"
+local jsonc = require "luci.jsonc"
+local cursor = luci.model.uci.cursor()
 local appname = "passwall_server"
 local a,t,e
-local m, s
-local o=require"nixio.fs"
-local methods={
-"none",
-"table",
-"rc4",
-"rc4-md5",
-"aes-128-cfb",
-"aes-192-cfb",
-"aes-256-cfb",
-"aes-128-ctr",
-"aes-192-ctr",
-"aes-256-ctr",
-"bf-cfb",
-"cast5-cfb",
-"des-cfb",
-"rc2-cfb",
-"salsa20",
-"chacha20",
-"chacha20-ietf",
-}
-local protocols={
-"origin",
-"verify_simple",
-"verify_deflate",
-"verify_sha1",
-"auth_simple",
-"auth_sha1",
-"auth_sha1_v2",
-"auth_sha1_v4",
-"auth_aes128_md5",
-"auth_aes128_sha1",
-"auth_chain_a",
-"auth_chain_b",
-"auth_chain_c",
-"auth_chain_d",
-}
-local obfss={
-"plain",
-"http_simple",
-"http_post",
-"random_head",
-"tls_simple",
-"tls1.0_session_auth",
-"tls1.2_ticket_auth",
-}
 
-a= Map(appname, translate("ShadowsocksR Python Server"))
-a.description = translate("")
-a.template=appname.."/ssr_python_server"
+local mudbjson = jsonc.parse(luci.sys.exec("cat /usr/share/ssr_python/mudb.json"))
 
-t=a:section(TypedSection,"global",translate("Global Setting"))
+a=Map(appname, translate("ShadowsocksR Python Server"))
+
+t=a:section(TypedSection,"global",translate("Global Settings"))
 t.anonymous=true
 t.addremove=false
 
-o=t:option(DummyValue,"ssr_python_status",translate("Current Condition"))
-o.template=appname.."/ssr_python_server_status"
-o.value=translate("Collecting data...")
+e=t:option(DummyValue,"status",translate("Current Condition"))
+e.template=appname.."/ssr_python_status"
+e.value=translate("Collecting data...")
 
 e=t:option(Flag,"ssr_python_enable",translate("Enable"))
 e.rmempty=false
 
-t=a:section(TypedSection,"ssr_python_server",translate("General Settings"))
+t=a:section(TypedSection,"ssr_python_users",translate("Users Manager"))
 t.anonymous=true
-t.addremove=false
+t.addremove=true
+t.template="cbi/tblsection"
+t.extedit=o.build_url("admin","vpn",appname,"ssr_python_config","%s")
+function t.create(e,t)
+	local e=TypedSection.create(e,t)
+	luci.http.redirect(o.build_url("admin","vpn",appname,"ssr_python_config",e))
+end
 
-e=t:option(ListValue,"encrypt_method",translate("Encrypt Method"))
-for a,t in ipairs(methods)do e:value(t)end
-e.rmempty=false
+function t.remove(t,a)
+	t.map.proceed=true
+	t.map:del(a)
+	luci.http.redirect(o.build_url("admin","vpn",appname,"ssr_python"))
+end
 
-e=t:option(ListValue,"protocol",translate("Protocol"))
-for a,t in ipairs(protocols)do e:value(t)end
-e.rmempty=false
+e=t:option(Flag, "enable", translate("Enable"))
+e.width="5%"
+e.rmempty = false
 
-e=t:option(Value,"protocol_param",translate("Protocol Param"))
+e=t:option(DummyValue,"remarks",translate("Remarks"))
+e.width="10%"
 
-e=t:option(ListValue,"obfs",translate("Obfs"))
-for a,t in ipairs(obfss)do e:value(t)end
-e.rmempty=false
+e=t:option(DummyValue,"port",translate("Port"))
+e.width="10%"
 
-e=t:option(Value,"obfs_param",translate("Obfs Param"))
+e=t:option(DummyValue,"forbidden_port",translate("Forbidden Port"))
+e.width="10%"
+e.cfgvalue=function(t,n)
+	local str="无"
+	local forbidden_port = a.uci:get(appname,n,"forbidden_port")
+	if forbidden_port then
+		str=forbidden_port
+	end
+	return str
+end
 
-e=t:option(DynamicList,"redirect",translate("redirect"),translate("Such as")..": *:8388#1.1.1.1:8388")
-e.placeholder = "*:8388#1.1.1.1:8388"
+e=t:option(DummyValue,"device_limit",translate("Device Limit"))
+e.width="10%"
 
-e=t:option(Value,"timeout",translate("Time Out"))
-e.datatype="uinteger"
-e.default=300
+e=t:option(DummyValue,"speed_limit_per_con",translate("Speed Limit Per Con"))
+e.width="10%"
+e.cfgvalue=function(t,n)
+	return a.uci:get(appname,n,"speed_limit_per_con").."Kb/s"
+end
 
-e=t:option(ListValue,"fast_open",translate("Fast Open"))
-e:value("false")
-e:value("true")
-e.default='false'
+e=t:option(DummyValue,"speed_limit_per_user",translate("Speed Limit Per User"))
+e.width="10%"
+e.cfgvalue=function(t,n)
+	return a.uci:get(appname,n,"speed_limit_per_user").."Kb/s"
+end
 
-e=a:section(TypedSection,"ssr_python_server_users",translate("Users Manager"))
-e.addremove=true
-e.anonymous=true
-e.template="cbi/tblsection"
-o=e:option(Flag,"enabled",translate("Enabled"))
-o.rmempty=false
-o=e:option(Value,"port",translate("Port"))
-o.datatype="port"
-o.rmempty=false
-o=e:option(Value,"password",translate("Password"))
-o.rmempty=false
+e=t:option(DummyValue,"transfer_enable",translate("Available Total Flow"))
+e.width="10%"
+e.cfgvalue=function(t,n)
+	return a.uci:get(appname,n,"transfer_enable").."G"
+end
+
+--[[e=t:option(DummyValue,"u",translate("Used Upload Traffic"))
+e.width="10%"
+e.cfgvalue=function(t,section)
+	local result = "未知"
+	if mudbjson then
+		for index,object in pairs(mudbjson) do
+			if object.id == section then
+				local u = object.u
+				if u < 1024 then result = u.."B"
+				elseif u < 1024*1024 then result = math.ceil((u/1024)).."KB"
+				elseif u < 1024*1024*1024 then result = math.ceil((u/1024/1024)).."MB"
+				elseif u < 1024*1024*1024*1024 then result = math.ceil((u/1024/1024/1024)).."TB" end
+			end
+		end
+	end
+	return result
+end
+
+e=t:option(DummyValue,"d",translate("Used Download Traffic"))
+e.width="10%"
+e.cfgvalue=function(t,section)
+	local result = "未知"
+	if mudbjson then
+		for index,object in pairs(mudbjson) do
+			if object.id == section then
+				local d = object.d
+				if d < 1024 then result = d.."B"
+				elseif d < 1024*1024 then result = math.ceil((d/1024)).."KB"
+				elseif d < 1024*1024*1024 then result = math.ceil((d/1024/1024)).."MB"
+				elseif d < 1024*1024*1024*1024 then result = math.ceil((d/1024/1024/1024)).."TB" end
+			end
+		end
+	end
+	return result
+end]]--
+
+e=t:option(DummyValue,"used_total_traffic",translate("Used Total traffic"))
+e.width="10%"
+e.cfgvalue=function(t,section)
+	local result = "未知"
+	if mudbjson then
+		for index,object in pairs(mudbjson) do
+			if object.id == section then
+				local count = object.d + object.u
+				if count < 1024 then result = count.."B"
+				elseif count < 1024*1024 then result = math.ceil((count/1024)).."KB"
+				elseif count < 1024*1024*1024 then result = math.ceil((count/1024/1024)).."MB"
+				elseif count < 1024*1024*1024*1024 then result = math.ceil((count/1024/1024/1024)).."TB" end
+			end
+		end
+	end
+	return result
+end
+
+e=t:option(DummyValue,"status",translate("Status"))
+e.template="passwall_server/ssr_python_users_status"
+e.width="20%"
+
+a:append(Template(appname.."/ssr_python"))
+a:append(Template(appname.."/ssr_python_users_list_status"))
 
 return a
