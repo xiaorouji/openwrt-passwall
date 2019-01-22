2f85767f5622d982f6c5b08268e5a9748cc0c1f1
Lienol <lienol@qq.com>
2019-01-22 21:02:31 +0800

    Update server-center
    Fix some bug
    Add clear traffic button
    Add auto clear traffic

--- lienol/luci-app-server-center/luasrc/model/cbi/server_center/ssr_python.lua
+++ lienol/luci-app-server-center/luasrc/model/cbi/server_center/ssr_python.lua
@@ -1,12 +1,12 @@
 local o = require "luci.dispatcher"
 local fs = require "nixio.fs"
 local sys = require "luci.sys"
-local jsonc = require "luci.jsonc"
+local cjson = require "cjson"
 local cursor = luci.model.uci.cursor()
 local appname = "server_center"
 local a,t,e
 
-local mudbjson = jsonc.parse(luci.sys.exec("cat /usr/share/ssr_python/mudb.json"))
+local mudbjson = cjson.decode(luci.sys.exec("cat /usr/share/ssr_python/mudb.json"))
 
 a=Map(appname, translate("ShadowsocksR Python Server"))
 
@@ -20,6 +20,21 @@ e.value=translate("Collecting data...")
 
 e=t:option(Flag,"ssr_python_enable",translate("Enable"))
 e.rmempty=false
+
+e=t:option(Flag,"ssr_python_auto_clear_transfer",translate("Enable Auto Clear Traffic"))
+e.default=0
+e.rmempty=false
+
+e=t:option(Value,"ssr_python_auto_clear_transfer_time",translate("Clear Traffic Time Interval"),translate("*,*,*,*,* is Min Hour Day Mon Week"))
+e.default="0,2,1,*,*"
+e:depends("ssr_python_auto_clear_transfer",1)
+
+e=t:option(Button,"clear_transfer",translate("Clear All Users Traffic"))
+e.inputstyle="remove"
+function e.write(t,section)
+	local url=luci.dispatcher.build_url("admin", "vpn", "server_center", "ssr_python_clear_traffic_all_users")
+	e.description = "<script>if (confirm('确认吗？')==true){XHR.get('"..url.."',null,function(x,result){window.location.replace(window.location.href)})}</script>"
+end
 
 t=a:section(TypedSection,"ssr_python_users",translate("Users Manager"))
 t.anonymous=true
@@ -40,6 +55,10 @@ end
 e=t:option(Flag, "enable", translate("Enable"))
 e.width="5%"
 e.rmempty = false
+
+e=t:option(DummyValue,"status",translate("Status"))
+e.template="server_center/ssr_python_users_status"
+e.width="5%"
 
 e=t:option(DummyValue,"remarks",translate("Remarks"))
 e.width="10%"
@@ -64,32 +83,38 @@ e.width="10%"
 e=t:option(DummyValue,"speed_limit_per_con",translate("Speed Limit Per Con"))
 e.width="10%"
 e.cfgvalue=function(t,n)
-	local str = a.uci:get(appname,n,"speed_limit_per_con")
-	if tonumber(str) == 0 then
-		return translate("No Speed Limit")
+	local str = translate("No Speed Limit")
+	local speed_limit_per_con = a.uci:get(appname,n,"speed_limit_per_con")
+	if speed_limit_per_con and tonumber(speed_limit_per_con) > 0 then
+		speed_limit_per_con = tonumber(speed_limit_per_con)
+		if speed_limit_per_con < 1024 then str = speed_limit_per_con.."Kb/s"
+		elseif speed_limit_per_con < 1024*1024 then str = string.format("%0.2f",speed_limit_per_con/1024).."Mb/s" end
 	end
-	return str.."Kb/s"
+	return str
 end
 
 e=t:option(DummyValue,"speed_limit_per_user",translate("Speed Limit Per User"))
 e.width="10%"
 e.cfgvalue=function(t,n)
-	local str = a.uci:get(appname,n,"speed_limit_per_user")
-	if tonumber(str) == 0 then
-		return translate("No Speed Limit")
+	local str = translate("No Speed Limit")
+	local speed_limit_per_user = a.uci:get(appname,n,"speed_limit_per_user")
+	if speed_limit_per_user and tonumber(speed_limit_per_user) > 0 then
+		speed_limit_per_user = tonumber(speed_limit_per_user)
+		if speed_limit_per_user < 1024 then str = speed_limit_per_user.."Kb/s"
+		elseif speed_limit_per_user < 1024*1024 then str = string.format("%0.2f",speed_limit_per_user/1024).."Mb/s" end
 	end
-	return str.."Kb/s"
+	return str
 end
 
 e=t:option(DummyValue,"transfer_enable",translate("Available Total Flow"))
 e.width="10%"
 e.cfgvalue=function(t,section)
-	local result = a.uci:get(appname,section,"transfer_enable")
-	if result and tonumber(result) == 0 then
-		return translate("Infinite")
-	else
-		return result.."G"
+	local str = translate("Infinite")
+	local transfer_enable = a.uci:get(appname,n,"transfer_enable")
+	if transfer_enable and tonumber(transfer_enable) > 0 then
+		str = transfer_enable.."G"
 	end
+	return str
 end
 
 --[[e=t:option(DummyValue,"u",translate("Used Upload Traffic"))
@@ -103,7 +128,8 @@ e.cfgvalue=function(t,section)
 				if u < 1024 then result = u.."B"
 				elseif u < 1024*1024 then result = string.format("%0.2f",(u/1024)).."KB"
 				elseif u < 1024*1024*1024 then result = string.format("%0.2f",(u/1024/1024)).."MB"
-				elseif u < 1024*1024*1024*1024 then result = string.format("%0.2f",(u/1024/1024/1024)).."GB" end
+				elseif u < 1024*1024*1024*1024 then result = string.format("%0.2f",(u/1024/1024/1024)).."GB"
+				elseif u < 1024*1024*1024*1024*1024 then result = string.format("%0.2f",(u/1024/1024/1024/1024)).."TB" end
 			end
 		end
 	end
@@ -121,14 +147,15 @@ e.cfgvalue=function(t,section)
 				if d < 1024 then result = d.."B"
 				elseif d < 1024*1024 then result = string.format("%0.2f",(d/1024)).."KB"
 				elseif d < 1024*1024*1024 then result = string.format("%0.2f",(d/1024/1024)).."MB"
-				elseif d < 1024*1024*1024*1024 then result = string.format("%0.2f",(d/1024/1024/1024)).."GB" end
+				elseif d < 1024*1024*1024*1024 then result = string.format("%0.2f",(d/1024/1024/1024)).."GB"
+				elseif d < 1024*1024*1024*1024*1024 then result = string.format("%0.2f",(d/1024/1024/1024/1024)).."TB" end
 			end
 		end
 	end
 	return result
 end]]--
 
-e=t:option(DummyValue,"used_total_traffic",translate("Used Total traffic"))
+e=t:option(DummyValue,"used_total_traffic",translate("Used Total Traffic"))
 e.width="10%"
 e.cfgvalue=function(t,section)
 	local result = translate("Null")
@@ -139,18 +166,22 @@ e.cfgvalue=function(t,section)
 				if count < 1024 then result = count.."B"
 				elseif count < 1024*1024 then result = string.format("%0.2f",(count/1024)).."KB"
 				elseif count < 1024*1024*1024 then result = string.format("%0.2f",(count/1024/1024)).."MB"
-				elseif count < 1024*1024*1024*1024 then result = string.format("%0.2f",(count/1024/1024/1024)).."GB" end
+				elseif count < 1024*1024*1024*1024 then result = string.format("%0.2f",(count/1024/1024/1024)).."GB"
+				elseif count < 1024*1024*1024*1024*1024 then result = string.format("%0.2f",(count/1024/1024/1024/1024)).."TB" end
 			end
 		end
 	end
 	return result
 end
 
-e=t:option(DummyValue,"status",translate("Status"))
-e.template="server_center/ssr_python_users_status"
-e.width="20%"
+e=t:option(Button,"clear_transfer",translate("Clear Traffic"))
+e.inputstyle="remove"
+function e.write(t,section)
+	local url=luci.dispatcher.build_url("admin", "vpn", "server_center", "ssr_python_clear_traffic")
+	e.description = "<script>if (confirm('确认吗？')==true){XHR.get('"..url.."',{id:'"..section.."'},function(x,result){window.location.replace(window.location.href)})}</script>"
+end
 
 a:append(Template(appname.."/ssr_python"))
-a:append(Template(appname.."/ssr_python_users_list_status"))
+--a:append(Template(appname.."/ssr_python_users_list_status"))
 
 return a
