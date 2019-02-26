2f6a4852df9ec61dea3b7a8ae09087054ecbbd25
Lienol <lawlienol@gmail.com>
2019-02-26 18:04:31 +0800

    Update NAS app

--- lienol/luci-app-filebrowser-mini/luasrc/controller/filebrowser-mini.lua
+++ lienol/luci-app-filebrowser-mini/luasrc/controller/filebrowser-mini.lua
@@ -2,23 +2,23 @@ module("luci.controller.filebrowser-mini", package.seeall)
 
 function index()
 
-	page = entry({"admin", "system", "filebrowser-mini"}, template("filebrowser-mini"), _("File Browser Mini"), 89)
+	page = entry({"admin", "nas", "filebrowser-mini"}, template("filebrowser-mini"), _("File Browser Mini"), 1)
 	page.i18n = "base"
 	page.dependent = true
 
-	page = entry({"admin", "system", "filebrowser-mini_list"}, call("filebrowser_list"), nil)
+	page = entry({"admin", "nas", "filebrowser-mini_list"}, call("filebrowser_list"), nil)
 	page.leaf = true
 
-	page = entry({"admin", "system", "filebrowser-mini_open"}, call("filebrowser_open"), nil)
+	page = entry({"admin", "nas", "filebrowser-mini_open"}, call("filebrowser_open"), nil)
 	page.leaf = true
 
-    page = entry({"admin", "system", "filebrowser-mini_delete"}, call("filebrowser_delete"), nil)
+    page = entry({"admin", "nas", "filebrowser-mini_delete"}, call("filebrowser_delete"), nil)
     page.leaf = true
 
-    page = entry({"admin", "system", "filebrowser-mini_rename"}, call("filebrowser_rename"), nil)
+    page = entry({"admin", "nas", "filebrowser-mini_rename"}, call("filebrowser_rename"), nil)
     page.leaf = true
 
-    page = entry({"admin", "system", "filebrowser-mini_upload"}, call("filebrowser_upload"), nil)
+    page = entry({"admin", "nas", "filebrowser-mini_upload"}, call("filebrowser_upload"), nil)
     page.leaf = true
 
 end
@@ -74,7 +74,7 @@ function filebrowser_upload()
     local filename = luci.http.formvalue("upload-filename")
     local uploaddir = luci.http.formvalue("upload-dir")
     local filepath = uploaddir..filename
-    local url = luci.dispatcher.build_url('admin', 'system', 'filebrowser')
+    local url = luci.dispatcher.build_url('admin', 'nas', 'filebrowser')
 
     local fp
     fp = io.open(filepath, "w")
