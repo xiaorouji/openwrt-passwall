6da2f7d1c1469823aa295fb6a263b9a2208ebe67
Lienol <lawlienol@gmail.com>
2019-04-30 23:03:08 +0800

    remove koolproxy

--- lienol/luci-app-koolproxy/files/usr/lib/lua/luci/controller/koolproxy.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.koolproxy",package.seeall)
-function index()
-	if not nixio.fs.access("/etc/config/koolproxy")then
-		return
-	end
-	entry({"admin","services","koolproxy"},cbi("koolproxy/global"),_("KoolProxy"),1).dependent=true
-	entry({"admin","services","koolproxy","rss_rule"},cbi("koolproxy/rss_rule"), nil).leaf=true
-end
