9d6c9153ebad9e82d0d0bd3ba6c43922c5d1de33
Lienol <lawlienol@gmail.com>
2019-07-04 01:23:47 +0800

    Fixed Buggggggg!!!!!!!

---
+++ lienol/luci-app-passwall/luasrc/model/cbi/passwall/acl.lua
@@ -0,0 +1,70 @@
+local sys = require "luci.sys"
+local webadmin = require "luci.tools.webadmin"
+
+m = Map("passwall")
+
+-- [[ ACLs Settings ]]--
+s = m:section(TypedSection, "acl_rule", translate("ACLs"), translate(
+                  "ACLs is a tools which used to designate specific IP proxy mode"))
+s.template = "cbi/tblsection"
+s.sortable = true
+s.anonymous = true
+s.addremove = true
+
+---- Enable
+o = s:option(Flag, "enabled", translate("Enable"))
+o.rmempty = false
+
+---- ACL Remarks
+o = s:option(Value, "aclremarks", translate("ACL Remarks"))
+o.rmempty = true
+
+---- IP Address
+o = s:option(Value, "ipaddr", translate("IP Address"))
+o.datatype = "ip4addr"
+o.rmempty = true
+
+local temp = {}
+for index, n in ipairs(luci.ip.neighbors({family = 4})) do
+    if n.dest then temp[index] = n.dest:string() end
+end
+local ips = {}
+for _, key in pairs(temp) do table.insert(ips, key) end
+table.sort(ips)
+
+for index, key in pairs(ips) do o:value(key, temp[key]) end
+-- webadmin.cbi_add_knownips(o)
+
+---- MAC Address
+o = s:option(Value, "macaddr", translate("MAC Address"))
+o.rmempty = true
+sys.net.mac_hints(function(e, t) o:value(e, "%s (%s)" % {e, t}) end)
+
+---- Proxy Mode
+o = s:option(ListValue, "proxy_mode", translate("Proxy Mode"))
+o.default = "disable"
+o.rmempty = false
+o:value("disable", translate("No Proxy"))
+o:value("global", translate("Global Proxy"))
+o:value("gfwlist", translate("GFW List"))
+o:value("chnroute", translate("China WhiteList"))
+o:value("gamemode", translate("Game Mode"))
+o:value("returnhome", translate("Return Home"))
+
+---- TCP Redir Ports
+o = s:option(Value, "tcp_redir_ports", translate("TCP Redir Ports"))
+o.default = "default"
+o:value("default", translate("Default"))
+o:value("1:65535", translate("All"))
+o:value("80,443", "80,443")
+o:value("80:", "80 " .. translate("or more"))
+o:value(":443", "443 " .. translate("or less"))
+
+---- UDP Redir Ports
+o = s:option(Value, "udp_redir_ports", translate("UDP Redir Ports"))
+o.default = "default"
+o:value("default", translate("Default"))
+o:value("1:65535", translate("All"))
+o:value("53", "53")
+
+return m
