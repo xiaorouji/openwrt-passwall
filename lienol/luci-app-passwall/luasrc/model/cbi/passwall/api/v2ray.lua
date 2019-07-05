fe0a15fb1ae419e66f99f45f60da11bed04ae4ef
Lienol <lawlienol@gmail.com>
2019-07-06 01:58:56 +0800

    Fixed bug

--- lienol/luci-app-passwall/luasrc/model/cbi/passwall/api/v2ray.lua
+++ lienol/luci-app-passwall/luasrc/model/cbi/passwall/api/v2ray.lua
@@ -5,6 +5,7 @@ local uci = require"luci.model.uci".cursor()
 local util = require "luci.util"
 local i18n = require "luci.i18n"
 local ipkg = require "luci.model.ipkg"
+local api = require "luci.model.cbi.passwall.api.api"
 
 local v2ray_api =
     "https://api.github.com/repos/v2ray/v2ray-core/releases/latest"
@@ -12,186 +13,47 @@ local wget = "/usr/bin/wget"
 local wget_args = {
     "--no-check-certificate", "--quiet", "--timeout=100", "--tries=3"
 }
-local curl = "/usr/bin/curl"
 local command_timeout = 300
 
 local LEDE_BOARD = nil
 local DISTRIB_TARGET = nil
 local is_armv7 = false
 
-local function _unpack(t, i)
-    i = i or 1
-    if t[i] ~= nil then return t[i], _unpack(t, i + 1) end
-end
-
-local function exec(cmd, args, writer, timeout)
-    local os = require "os"
-    local nixio = require "nixio"
-
-    local fdi, fdo = nixio.pipe()
-    local pid = nixio.fork()
-
-    if pid > 0 then
-        fdo:close()
-
-        if writer or timeout then
-            local starttime = os.time()
-            while true do
-                if timeout and os.difftime(os.time(), starttime) >= timeout then
-                    nixio.kill(pid, nixio.const.SIGTERM)
-                    return 1
-                end
-
-                if writer then
-                    local buffer = fdi:read(2048)
-                    if buffer and #buffer > 0 then
-                        writer(buffer)
-                    end
-                end
-
-                local wpid, stat, code = nixio.waitpid(pid, "nohang")
-
-                if wpid and stat == "exited" then return code end
-
-                if not writer and timeout then nixio.nanosleep(1) end
-            end
-        else
-            local wpid, stat, code = nixio.waitpid(pid)
-            return wpid and stat == "exited" and code
-        end
-    elseif pid == 0 then
-        nixio.dup(fdo, nixio.stdout)
-        fdi:close()
-        fdo:close()
-        nixio.exece(cmd, args, nil)
-        nixio.stdout:close()
-        os.exit(1)
-    end
-end
-
-local function compare_versions(ver1, comp, ver2)
-    local table = table
-
-    local av1 = util.split(ver1, "[%.%-]", nil, true)
-    local av2 = util.split(ver2, "[%.%-]", nil, true)
-
-    local max = table.getn(av1)
-    local n2 = table.getn(av2)
-    if (max < n2) then max = n2 end
-
-    for i = 1, max, 1 do
-        local s1 = av1[i] or ""
-        local s2 = av2[i] or ""
-
-        if comp == "~=" and (s1 ~= s2) then return true end
-        if (comp == "<" or comp == "<=") and (s1 < s2) then return true end
-        if (comp == ">" or comp == ">=") and (s1 > s2) then return true end
-        if (s1 ~= s2) then return false end
-    end
-
-    return not (comp == "<" or comp == ">")
-end
-
-local function auto_get_arch()
-    local arch = nixio.uname().machine or ""
-    if fs.access("/usr/lib/os-release") then
-        LEDE_BOARD = sys.exec(
-                         "grep 'LEDE_BOARD' /usr/lib/os-release | awk -F '[\'\"]' '{print $2}'")
-    end
-    if fs.access("/etc/openwrt_release") then
-        DISTRIB_TARGET = sys.exec(
-                             'grep "DISTRIB_TARGET" /etc/openwrt_release | awk -F "[\'\"]" \'{print $2}\'')
-    end
-
-    if arch == "mips" then
-        if LEDE_BOARD ~= "" then
-            arch = sys.exec("echo '" .. LEDE_BOARD ..
-                                "' | grep -oE 'ramips|ar71xx'")
-        elseif DISTRIB_TARGET ~= "" then
-            arch = sys.exec("echo '" .. DISTRIB_TARGET ..
-                                "' | grep -oE 'ramips|ar71xx'")
-        end
-    end
-
-    return util.trim(arch)
-end
-
-local function get_file_info(arch)
-    local file_tree = ""
-    local sub_version = ""
-
-    if arch == "x86_64" then
-        file_tree = "64"
-    elseif arch == "aarch64" then
-        file_tree = "arm64"
-    elseif arch == "ramips" then
-        file_tree = "mipsle"
-    elseif arch == "ar71xx" then
-        file_tree = "mips"
-    elseif arch:match("^i[%d]86$") then
-        file_tree = "32"
-    elseif arch:match("^armv[5-8]") then
-        file_tree = "arm"
-        sub_version = arch:match("[5-8]")
-        if LEDE_BOARD and string.match(LEDE_BOARD, "bcm53xx") == "bcm53xx" then
-            sub_version = "5"
-        elseif DISTRIB_TARGET and string.match(DISTRIB_TARGET, "bcm53xx") ==
-            "bcm53xx" then
-            sub_version = "5"
-        end
-        sub_version = "5"
-        if sub_version == "7" then is_armv7 = true end
-    end
-
-    return file_tree, sub_version
-end
-
-local function get_api_json(url)
-    local jsonc = require "luci.jsonc"
-
-    local output = {}
-    -- exec(wget, { "-O-", url, _unpack(wget_args) },
-    --	function(chunk) output[#output + 1] = chunk end)
-    -- local json_content = util.trim(table.concat(output))
-
-    local json_content = luci.sys.exec(curl .. " -sL " .. url)
-
-    if json_content == "" then return {} end
-
-    return jsonc.parse(json_content) or {}
-end
-
 function get_v2ray_file_path()
-    return uci:get("passwall", "global_v2ray", "v2ray_client_file") or
-               luci.sys.exec(
-                   "echo -n `uci get passwall.@global_v2ray[0].v2ray_client_file`")
+    return uci:get("passwall", "global_app", "v2ray_client_file") or
+               sys.exec(
+                   "echo -n `uci get passwall.@global_app[0].v2ray_client_file`")
 end
 
 function get_v2ray_version()
     if get_v2ray_file_path() and get_v2ray_file_path() ~= "" then
         if fs.access(get_v2ray_file_path() .. "/v2ray") then
-            return luci.sys.exec("echo -n `" .. get_v2ray_file_path() ..
-                                     "/v2ray -version | awk '{print $2}' | sed -n 1P" ..
-                                     "`")
+            return sys.exec("echo -n `" .. get_v2ray_file_path() ..
+                                "/v2ray -version | awk '{print $2}' | sed -n 1P" ..
+                                "`")
         end
     end
     return ""
 end
 
 function to_check(arch)
-    if not arch or arch == "" then arch = auto_get_arch() end
+    if not arch or arch == "" then arch = api.auto_get_arch() end
 
-    local file_tree, sub_version = get_file_info(arch)
+    local file_tree, sub_version = api.get_file_info(arch)
+    if sub_version == "7" then is_armv7 = true end
 
     if file_tree == "" then
         return {
             code = 1,
             error = i18n.translate(
-                "Can't determine ARCH, or ARCH not supported. Please select manually.")
+                "Can't determine ARCH, or ARCH not supported.")
         }
     end
 
-    local json = get_api_json(v2ray_api)
+    if file_tree == "amd64" then file_tree = "64" end
+    if file_tree == "386" then file_tree = "32" end
+
+    local json = api.get_api_json(v2ray_api)
 
     if json.tag_name == nil then
         return {
@@ -201,8 +63,8 @@ function to_check(arch)
     end
 
     local remote_version = json.tag_name:match("[^v]+")
-    local needs_update = compare_versions(get_v2ray_version(), "<",
-                                          remote_version)
+    local needs_update = api.compare_versions(get_v2ray_version(), "<",
+                                              remote_version)
     local html_url, download_url
 
     if needs_update then
@@ -244,11 +106,11 @@ function to_download(url)
 
     local tmp_file = util.trim(util.exec("mktemp -u -t v2ray_download.XXXXXX"))
 
-    local result = exec(wget, {"-O", tmp_file, url, _unpack(wget_args)}, nil,
-                        command_timeout) == 0
+    local result = api.exec(wget, {"-O", tmp_file, url, api._unpack(wget_args)},
+                            nil, command_timeout) == 0
 
     if not result then
-        exec("/bin/rm", {"-f", tmp_file})
+        api.exec("/bin/rm", {"-f", tmp_file})
         return {
             code = 1,
             error = i18n.translatef("File download failed or timed out: %s", url)
@@ -273,12 +135,12 @@ function to_extract(file, subfix)
     local tmp_dir = util.trim(util.exec("mktemp -d -t v2ray_extract.XXXXXX"))
 
     local output = {}
-    exec("/usr/bin/unzip", {"-o", file, "-d", tmp_dir},
-         function(chunk) output[#output + 1] = chunk end)
+    api.exec("/usr/bin/unzip", {"-o", file, "-d", tmp_dir},
+             function(chunk) output[#output + 1] = chunk end)
 
     local files = util.split(table.concat(output))
 
-    exec("/bin/rm", {"-f", file})
+    api.exec("/bin/rm", {"-f", file})
 
     return {code = 0, file = tmp_dir}
 end
@@ -293,17 +155,18 @@ function to_move(file)
 
     sys.call("mkdir -p " .. client_file)
 
-    if not arch or arch == "" then arch = auto_get_arch() end
-    local file_tree, sub_version = get_file_info(arch)
+    if not arch or arch == "" then arch = api.auto_get_arch() end
+    local file_tree, sub_version = api.get_file_info(arch)
+    if sub_version == "7" then is_armv7 = true end
     local result = nil
     if is_armv7 and is_armv7 == true then
-        result = exec("/bin/mv", {
+        result = api.exec("/bin/mv", {
             "-f", file .. "/v2ray_armv7", file .. "/v2ctl_armv7", client_file
         }, nil, command_timeout) == 0
     else
-        result = exec("/bin/mv",
-                      {"-f", file .. "/v2ray", file .. "/v2ctl", client_file},
-                      nil, command_timeout) == 0
+        result = api.exec("/bin/mv", {
+            "-f", file .. "/v2ray", file .. "/v2ctl", client_file
+        }, nil, command_timeout) == 0
     end
     if not result or not fs.access(client_file) then
         sys.call("/bin/rm -rf /tmp/v2ray_extract.*")
@@ -314,7 +177,7 @@ function to_move(file)
         }
     end
 
-    exec("/bin/chmod", {"-R", "755", client_file})
+    api.exec("/bin/chmod", {"-R", "755", client_file})
 
     sys.call("/bin/rm -rf /tmp/v2ray_extract.*")
 
