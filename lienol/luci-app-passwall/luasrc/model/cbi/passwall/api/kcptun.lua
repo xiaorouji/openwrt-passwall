7c42045718fd4a0584351b473e3d66cd8fa36d8a
Lienol <lawlienol@gmail.com>
2019-07-06 01:58:56 +0800

    Fixed bug

--- lienol/luci-app-passwall/luasrc/model/cbi/passwall/api/kcptun.lua
+++ lienol/luci-app-passwall/luasrc/model/cbi/passwall/api/kcptun.lua
@@ -5,6 +5,7 @@ local uci = require"luci.model.uci".cursor()
 local util = require "luci.util"
 local i18n = require "luci.i18n"
 local ipkg = require "luci.model.ipkg"
+local api = require "luci.model.cbi.passwall.api.api"
 
 local kcptun_api = "https://api.github.com/repos/xtaci/kcptun/releases/latest"
 
@@ -12,157 +13,15 @@ local wget = "/usr/bin/wget"
 local wget_args = {
     "--no-check-certificate", "--quiet", "--timeout=100", "--tries=3"
 }
-local curl = "/usr/bin/curl"
 local command_timeout = 300
 
 local LEDE_BOARD = nil
 local DISTRIB_TARGET = nil
 
-local function _unpack(t, i)
-    i = i or 1
-    if t[i] ~= nil then return t[i], _unpack(t, i + 1) end
-end
-
-local function exec(cmd, args, writer, timeout)
-    local os = require "os"
-    local nixio = require "nixio"
-
-    local fdi, fdo = nixio.pipe()
-    local pid = nixio.fork()
-
-    if pid > 0 then
-        fdo:close()
-
-        if writer or timeout then
-            local starttime = os.time()
-            while true do
-                if timeout and os.difftime(os.time(), starttime) >= timeout then
-                    nixio.kill(pid, nixio.const.SIGTERM)
-                    return 1
-                end
-
-                if writer then
-                    local buffer = fdi:read(2048)
-                    if buffer and #buffer > 0 then
-                        writer(buffer)
-                    end
-                end
-
-                local wpid, stat, code = nixio.waitpid(pid, "nohang")
-
-                if wpid and stat == "exited" then return code end
-
-                if not writer and timeout then nixio.nanosleep(1) end
-            end
-        else
-            local wpid, stat, code = nixio.waitpid(pid)
-            return wpid and stat == "exited" and code
-        end
-    elseif pid == 0 then
-        nixio.dup(fdo, nixio.stdout)
-        fdi:close()
-        fdo:close()
-        nixio.exece(cmd, args, nil)
-        nixio.stdout:close()
-        os.exit(1)
-    end
-end
-
-local function compare_versions(ver1, comp, ver2)
-    local table = table
-
-    local av1 = util.split(ver1, "[%.%-]", nil, true)
-    local av2 = util.split(ver2, "[%.%-]", nil, true)
-
-    local max = table.getn(av1)
-    local n2 = table.getn(av2)
-    if (max < n2) then max = n2 end
-
-    for i = 1, max, 1 do
-        local s1 = av1[i] or ""
-        local s2 = av2[i] or ""
-
-        if comp == "~=" and (s1 ~= s2) then return true end
-        if (comp == "<" or comp == "<=") and (s1 < s2) then return true end
-        if (comp == ">" or comp == ">=") and (s1 > s2) then return true end
-        if (s1 ~= s2) then return false end
-    end
-
-    return not (comp == "<" or comp == ">")
-end
-
-local function auto_get_arch()
-    local arch = nixio.uname().machine or ""
-    if fs.access("/usr/lib/os-release") then
-        LEDE_BOARD = sys.exec(
-                         "grep 'LEDE_BOARD' /usr/lib/os-release | awk -F '[\'\"]' '{print $2}'")
-    end
-    if fs.access("/etc/openwrt_release") then
-        DISTRIB_TARGET = sys.exec(
-                             'grep "DISTRIB_TARGET" /etc/openwrt_release | awk -F "[\'\"]" \'{print $2}\'')
-    end
-
-    if arch == "mips" then
-        if LEDE_BOARD ~= "" then
-            arch = sys.exec("echo '" .. LEDE_BOARD ..
-                                "' | grep -oE 'ramips|ar71xx'")
-        elseif DISTRIB_TARGET ~= "" then
-            arch = sys.exec("echo '" .. DISTRIB_TARGET ..
-                                "' | grep -oE 'ramips|ar71xx'")
-        end
-    end
-
-    return util.trim(arch)
-end
-
-local function get_file_info(arch)
-    local file_tree = ""
-    local sub_version = ""
-
-    if arch == "x86_64" then
-        file_tree = "amd64"
-    elseif arch == "aarch64" then
-        file_tree = "arm64"
-    elseif arch == "ramips" then
-        file_tree = "mipsle"
-    elseif arch == "ar71xx" then
-        file_tree = "mips"
-    elseif arch:match("^i[%d]86$") then
-        file_tree = "386"
-    elseif arch:match("^armv[5-8]") then
-        file_tree = "arm"
-        sub_version = arch:match("[5-8]")
-        if LEDE_BOARD and string.match(LEDE_BOARD, "bcm53xx") == "bcm53xx" then
-            sub_version = "5"
-        elseif DISTRIB_TARGET and string.match(DISTRIB_TARGET, "bcm53xx") ==
-            "bcm53xx" then
-            sub_version = "5"
-        end
-        sub_version = "5"
-    end
-
-    return file_tree, sub_version
-end
-
-local function get_api_json(url)
-    local jsonc = require "luci.jsonc"
-
-    local output = {}
-    -- exec(wget, { "-O-", url, _unpack(wget_args) },
-    --	function(chunk) output[#output + 1] = chunk end)
-    -- local json_content = util.trim(table.concat(output))
-
-    local json_content = luci.sys.exec(curl .. " -sL " .. url)
-
-    if json_content == "" then return {} end
-
-    return jsonc.parse(json_content) or {}
-end
-
 function get_kcptun_file_path()
-    return uci:get("passwall", "global_kcptun", "kcptun_client_file") or
+    return uci:get("passwall", "global_app", "kcptun_client_file") or
                luci.sys.exec(
-                   "echo -n `uci get passwall.@global_kcptun[0].kcptun_client_file`")
+                   "echo -n `uci get passwall.@global_app[0].kcptun_client_file`")
 end
 
 function get_kcptun_version(file)
@@ -185,19 +44,19 @@ function get_kcptun_version(file)
 end
 
 function to_check(arch)
-    if not arch or arch == "" then arch = auto_get_arch() end
+    if not arch or arch == "" then arch = api.auto_get_arch() end
 
-    local file_tree, sub_version = get_file_info(arch)
+    local file_tree, sub_version = api.get_file_info(arch)
 
     if file_tree == "" then
         return {
             code = 1,
             error = i18n.translate(
-                "Can't determine ARCH, or ARCH not supported. Please select manually.")
+                "Can't determine ARCH, or ARCH not supported.")
         }
     end
 
-    local json = get_api_json(kcptun_api)
+    local json = api.get_api_json(kcptun_api)
 
     if json.tag_name == nil then
         return {
@@ -210,8 +69,8 @@ function to_check(arch)
 
     local client_file = get_kcptun_file_path()
 
-    local needs_update = compare_versions(get_kcptun_version(client_file), "<",
-                                          remote_version)
+    local needs_update = api.compare_versions(get_kcptun_version(client_file),
+                                              "<", remote_version)
     local html_url, download_url
 
     if needs_update then
@@ -253,11 +112,11 @@ function to_download(url)
 
     local tmp_file = util.trim(util.exec("mktemp -u -t kcptun_download.XXXXXX"))
 
-    local result = exec(wget, {"-O", tmp_file, url, _unpack(wget_args)}, nil,
-                        command_timeout) == 0
+    local result = api.exec(wget, {"-O", tmp_file, url, api._unpack(wget_args)},
+                            nil, command_timeout) == 0
 
     if not result then
-        exec("/bin/rm", {"-f", tmp_file})
+        api.exec("/bin/rm", {"-f", tmp_file})
         return {
             code = 1,
             error = i18n.translatef("File download failed or timed out: %s", url)
@@ -276,12 +135,12 @@ function to_extract(file, subfix)
     local tmp_dir = util.trim(util.exec("mktemp -d -t kcptun_extract.XXXXXX"))
 
     local output = {}
-    exec("/bin/tar", {"-C", tmp_dir, "-zxvf", file},
-         function(chunk) output[#output + 1] = chunk end)
+    api.exec("/bin/tar", {"-C", tmp_dir, "-zxvf", file},
+             function(chunk) output[#output + 1] = chunk end)
 
     local files = util.split(table.concat(output))
 
-    exec("/bin/rm", {"-f", file})
+    api.exec("/bin/rm", {"-f", file})
 
     local new_file = nil
     for _, f in pairs(files) do
@@ -301,7 +160,7 @@ function to_extract(file, subfix)
     end
 
     if not new_file then
-        exec("/bin/rm", {"-rf", tmp_dir})
+        api.exec("/bin/rm", {"-rf", tmp_dir})
         return {
             code = 1,
             error = i18n.translatef("Can't find client in file: %s", file)
@@ -323,7 +182,7 @@ function to_move(file)
         return {
             code = 1,
             error = i18n.translate(
-                "The client file is not suitable for current device. Please reselect ARCH.")
+                "The client file is not suitable for current device.")
         }
     end
 
@@ -332,16 +191,16 @@ function to_move(file)
 
     if fs.access(client_file) then
         client_file_bak = client_file .. ".bak"
-        exec("/bin/mv", {"-f", client_file, client_file_bak})
+        api.exec("/bin/mv", {"-f", client_file, client_file_bak})
     end
 
-    local result = exec("/bin/mv", {"-f", file, client_file}, nil,
-                        command_timeout) == 0
+    local result = api.exec("/bin/mv", {"-f", file, client_file}, nil,
+                            command_timeout) == 0
 
     if not result or not fs.access(client_file) then
         sys.call("/bin/rm -rf /tmp/kcptun_extract.*")
         if client_file_bak then
-            exec("/bin/mv", {"-f", client_file_bak, client_file})
+            api.exec("/bin/mv", {"-f", client_file_bak, client_file})
         end
         return {
             code = 1,
@@ -350,9 +209,9 @@ function to_move(file)
         }
     end
 
-    exec("/bin/chmod", {"755", client_file})
+    api.exec("/bin/chmod", {"755", client_file})
 
-    if client_file_bak then exec("/bin/rm", {"-f", client_file_bak}) end
+    if client_file_bak then api.exec("/bin/rm", {"-f", client_file_bak}) end
 
     sys.call("/bin/rm -rf /tmp/kcptun_extract.*")
 
