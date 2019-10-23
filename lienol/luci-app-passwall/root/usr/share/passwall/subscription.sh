66b13624ea776cd655fa8ad24decc13cc8477a7b
Lienol <lawlienol@gmail.com>
2019-10-23 20:57:32 +0800

    passwall : optimize code

--- lienol/luci-app-passwall/root/usr/share/passwall/subscription.sh
+++ lienol/luci-app-passwall/root/usr/share/passwall/subscription.sh
@@ -17,19 +17,19 @@ start() {
 	#防止并发开启服务
 	[ -f "$LOCK_FILE" ] && return 3
 	touch "$LOCK_FILE"
-	echo "$Date: 开始执行在线订阅脚本..." >> $LOG_FILE
+	echo "$Date: 开始执行在线订阅脚本..." >>$LOG_FILE
 	/usr/share/$CONFIG/subscription_ssr.sh start 2>/dev/null
 	/usr/share/$CONFIG/subscription_v2ray.sh start 2>/dev/null
-	echo "$Date: 在线订阅脚本执行完毕..." >> $LOG_FILE
+	echo "$Date: 在线订阅脚本执行完毕..." >>$LOG_FILE
 	rm -f "$LOCK_FILE"
 	exit 0
 }
 
 stop() {
-	echo "$Date: 开始执行删除所有订阅脚本..." >> $LOG_FILE
+	echo "$Date: 开始执行删除所有订阅脚本..." >>$LOG_FILE
 	/usr/share/$CONFIG/subscription_ssr.sh stop 2>/dev/null
 	/usr/share/$CONFIG/subscription_v2ray.sh stop 2>/dev/null
-	echo "$Date: 删除所有订阅脚本执行完毕..." >> $LOG_FILE
+	echo "$Date: 删除所有订阅脚本执行完毕..." >>$LOG_FILE
 	rm -f "$LOCK_FILE"
 	exit 0
 }
