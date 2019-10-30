d2f6b86eab0ccfa6380f9d50ad4bd0f848306e9a
Lienol <lawlienol@gmail.com>
2019-10-30 14:36:25 +0800

    update fileassistant by filebrowser-mini

--- lienol/luci-app-fileassistant/luasrc/controller/fileassistant.lua
+++ lienol/luci-app-fileassistant/luasrc/controller/fileassistant.lua
@@ -1,25 +1,30 @@
-module("luci.controller.filebrowser_mini", package.seeall)
+module("luci.controller.fileassistant", package.seeall)
 
 function index()
 
-	entry({"admin", "nas"}, firstchild(), "NAS", 44).dependent = false
-    page = entry({"admin", "nas", "filebrowser_mini"}, template("filebrowser_mini"), _("迷你文件浏览器"), 1)
+    entry({"admin", "nas"}, firstchild(), "NAS", 44).dependent = false
+
+    local page
+    page = entry({"admin", "nas", "fileassistant"}, template("fileassistant"), _("文件助手"), 1)
     page.i18n = "base"
     page.dependent = true
 
-    page = entry({"admin", "nas", "filebrowser_mini_list"}, call("filebrowser_list"), nil)
+    page = entry({"admin", "nas", "fileassistant", "list"}, call("fileassistant_list"), nil)     
     page.leaf = true
 
-    page = entry({"admin", "nas", "filebrowser_mini_open"}, call("filebrowser_open"), nil)
+    page = entry({"admin", "nas", "fileassistant", "open"}, call("fileassistant_open"), nil)
     page.leaf = true
 
-    page = entry({"admin", "nas", "filebrowser_mini_delete"}, call("filebrowser_delete"), nil)
+    page = entry({"admin", "nas", "fileassistant", "delete"}, call("fileassistant_delete"), nil)
     page.leaf = true
 
-    page = entry({"admin", "nas", "filebrowser_mini_rename"}, call("filebrowser_rename"), nil)
+    page = entry({"admin", "nas", "fileassistant", "rename"}, call("fileassistant_rename"), nil)
     page.leaf = true
 
-    page = entry({"admin", "nas", "filebrowser_mini_upload"}, call("filebrowser_upload"), nil)
+    page = entry({"admin", "nas", "fileassistant", "upload"}, call("fileassistant_upload"), nil)
+    page.leaf = true
+
+    page = entry({"admin", "nas", "fileassistant", "install"}, call("fileassistant_install"), nil)
     page.leaf = true
 
 end
@@ -41,12 +46,12 @@ function list_response(path, success)
     luci.http.write_json(result)
 end
 
-function filebrowser_list()
+function fileassistant_list()
     local path = luci.http.formvalue("path")
     list_response(path, true)
 end
 
-function filebrowser_open()
+function fileassistant_open()
     local path = luci.http.formvalue("path")
     local filename = luci.http.formvalue("filename")
     local io = require "io"
@@ -60,7 +65,7 @@ function filebrowser_open()
     luci.ltn12.pump.all(luci.ltn12.source.file(download_fpi), luci.http.write)
 end
 
-function filebrowser_delete()
+function fileassistant_delete()
     local path = luci.http.formvalue("path")
     local isdir = luci.http.formvalue("isdir")
     path = path:gsub("<>", "/")
@@ -74,14 +79,37 @@ function filebrowser_delete()
     list_response(nixio.fs.dirname(path), success)
 end
 
-function filebrowser_rename()
+function fileassistant_rename()
     local filepath = luci.http.formvalue("filepath")
     local newpath = luci.http.formvalue("newpath")
     local success = os.execute('mv "'..filepath..'" "'..newpath..'"')
     list_response(nixio.fs.dirname(filepath), success)
 end
 
-function filebrowser_upload()
+function fileassistant_install()
+    local filepath = luci.http.formvalue("filepath")
+    local isdir = luci.http.formvalue("isdir")
+    local ext = filepath:match(".+%.(%w+)$")
+    filepath = filepath:gsub("<>", "/")
+    filepath = filepath:gsub(" ", "\ ")
+    local success
+    if isdir == "1" then
+        success = false  
+    elseif ext == "ipk" then
+        success = installIPK(filepath)
+    else
+        success = false
+    end
+    list_response(nixio.fs.dirname(filepath), success)
+end
+
+function installIPK(filepath)
+    luci.sys.exec('opkg --force-depends install "'..filepath..'"')
+    luci.sys.exec('rm -rf /tmp/luci-*')
+    return true;
+end
+
+function fileassistant_upload()
     local filecontent = luci.http.formvalue("upload-file")
     local filename = luci.http.formvalue("upload-filename")
     local uploaddir = luci.http.formvalue("upload-dir")
