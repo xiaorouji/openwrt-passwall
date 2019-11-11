3bad5cf10d8b3871231511c477a873c99cae18ab
Lienol <lawlienol@gmail.com>
2019-11-11 21:47:33 +0800

    passwall : fix bug
    after the big change, continue to fix bugs...
    git push for the last time today ...

--- lienol/luci-app-passwall/root/usr/share/passwall/monitor.sh
+++ lienol/luci-app-passwall/root/usr/share/passwall/monitor.sh
@@ -3,68 +3,92 @@
 CONFIG=passwall
 CONFIG_PATH=/var/etc/$CONFIG
 
-uci_get_by_name() {
+config_n_get() {
 	local ret=$(uci get $CONFIG.$1.$2 2>/dev/null)
 	echo ${ret:=$3}
 }
 
-uci_get_by_type() {
-	local ret=$(uci get $CONFIG.@$1[0].$2 2>/dev/null)
+config_t_get() {
+	local index=0
+	[ -n "$4" ] && index=$4
+	local ret=$(uci get $CONFIG.@$1[$index].$2 2>/dev/null)
 	echo ${ret:=$3}
 }
 
-TCP_REDIR_SERVER1=$(uci_get_by_type global tcp_redir_server1 nil)
-TCP_REDIR_PORT=$(uci_get_by_type global_proxy tcp_redir_port nil)
-UDP_REDIR_SERVER1=$(uci_get_by_type global udp_redir_server1 nil)
-UDP_REDIR_PORT=$(uci_get_by_type global_proxy udp_redir_port nil)
-[ "$UDP_REDIR_SERVER1" == "default" ] && UDP_REDIR_SERVER1=$TCP_REDIR_SERVER1
-SOCKS5_PROXY_SERVER1=$(uci_get_by_type global socks5_proxy_server1 nil)
-dns_mode=$(uci_get_by_type global dns_mode)
-use_haproxy=$(uci_get_by_type global_haproxy balancing_enable 0)
-use_kcp=$(uci_get_by_name $TCP_REDIR_SERVER1 use_kcp 0)
-kcp_port=$(uci_get_by_type global_proxy kcptun_port 11183)
+TCP_REDIR_SERVER_NUM=$(config_t_get global_other tcp_redir_server_num 1)
+for i in $(seq 1 $TCP_REDIR_SERVER_NUM); do
+	eval TCP_REDIR_SERVER$i=$(config_t_get global tcp_redir_server$i nil)
+done
+
+UDP_REDIR_SERVER_NUM=$(config_t_get global_other udp_redir_server_num 1)
+for i in $(seq 1 $UDP_REDIR_SERVER_NUM); do
+	eval UDP_REDIR_SERVER$i=$(config_t_get global udp_redir_server$i nil)
+done
+
+SOCKS5_PROXY_SERVER_NUM=$(config_t_get global_other socks5_proxy_server_num 1)
+for i in $(seq 1 $SOCKS5_PROXY_SERVER_NUM); do
+	eval SOCKS5_PROXY_SERVER$i=$(config_t_get global socks5_proxy_server$i nil)
+done
+
+dns_mode=$(config_t_get global dns_mode)
+use_haproxy=$(config_t_get global_haproxy balancing_enable 0)
 
 #tcp
-if [ $TCP_REDIR_SERVER1 != "nil" ]; then
-	icount=$(ps -w | grep -i -E "ss-redir|ssr-redir|v2ray|brook tproxy -l 0.0.0.0:$TCP_REDIR_PORT" | grep $CONFIG_PATH/TCP.json | grep -v grep | wc -l)
-	if [ $icount = 0 ]; then
-		/etc/init.d/passwall restart
-		exit 0
+for i in $(seq 1 $TCP_REDIR_SERVER_NUM); do
+	eval temp_server=\$TCP_REDIR_SERVER$i
+	if [ "$temp_server" != "nil" ]; then
+		#kcptun
+		use_kcp=$(config_n_get $temp_server use_kcp 0)
+		if [ $use_kcp -gt 0 ]; then
+			kcp_port=$(config_t_get global_proxy kcptun_port 11183)
+			icount=$(ps -w | grep kcptun-client | grep $kcp_port | grep -v grep | wc -l)
+			if [ $icount = 0 ]; then
+				/etc/init.d/passwall restart
+				exit 0
+			fi
+		fi
+		[ -f "/var/etc/passwall/port/TCP_$i" ] && listen_port=$(echo -n `cat /var/etc/passwall/port/TCP_$i`)
+		icount=$(ps -w | grep -v grep | grep -i -E "${CONFIG}/TCP_${i}|brook tproxy -l 0.0.0.0:${listen_port}" | wc -l)
+		if [ $icount = 0 ]; then
+			/etc/init.d/passwall restart
+			exit 0
+		fi
 	fi
-fi
+done
+
 
 #udp
-if [ $UDP_REDIR_SERVER1 != "nil" ]; then
-	icount=$(ps -w | grep -i -E "ss-redir|ssr-redir|v2ray|brook tproxy -l 0.0.0.0:$UDP_REDIR_PORT" | grep $CONFIG_PATH/UDP.json | grep -v grep | wc -l)
-	if [ $icount = 0 ]; then
-		/etc/init.d/passwall restart
-		exit 0
+for i in $(seq 1 $UDP_REDIR_SERVER_NUM); do
+	eval temp_server=\$UDP_REDIR_SERVER$i
+	if [ "$temp_server" != "nil" ]; then
+		[ "$temp_server" == "default" ] && temp_server=$TCP_REDIR_SERVER1
+		[ -f "/var/etc/passwall/port/UDP_$i" ] && listen_port=$(echo -n `cat /var/etc/passwall/port/UDP_$i`)
+		icount=$(ps -w | grep -v grep | grep -i -E "${CONFIG}/UDP_${i}|brook tproxy -l 0.0.0.0:${listen_port}" | wc -l)
+		if [ $icount = 0 ]; then
+			/etc/init.d/passwall restart
+			exit 0
+		fi
 	fi
-fi
+done
 
 #socks5
-if [ $SOCKS5_PROXY_SERVER1 != "nil" ]; then
-	icount=$(ps -w | grep -i -E "ss-redir|ssr-redir|v2ray|brook client" | grep $CONFIG_PATH/SOCKS5.json | grep -v grep | wc -l)
-	if [ $icount = 0 ]; then
-		/etc/init.d/passwall restart
-		exit 0
+for i in $(seq 1 $SOCKS5_PROXY_SERVER_NUM); do
+	eval temp_server=\$SOCKS5_PROXY_SERVER$i
+	if [ "$temp_server" != "nil" ]; then
+		[ -f "/var/etc/passwall/port/Socks5_$i" ] && listen_port=$(echo -n `cat /var/etc/passwall/port/Socks5_$i`)
+		icount=$(ps -w | grep -v grep | grep -i -E "${CONFIG}/Socks5_${i}|brook client -l 0.0.0.0:${listen_port}" | wc -l)
+		if [ $icount = 0 ]; then
+			/etc/init.d/passwall restart
+			exit 0
+		fi
 	fi
-fi
+done
 
 #dns
 icount=$(netstat -apn | grep 7913 | wc -l)
 if [ $icount = 0 ]; then
 	/etc/init.d/passwall restart
 	exit 0
-fi
-
-#kcptun
-if [ $use_kcp -gt 0 ]; then
-	icount=$(ps -w | grep kcptun_client | grep $kcp_port | grep -v grep | wc -l)
-	if [ $icount = 0 ]; then
-		/etc/init.d/passwall restart
-		exit 0
-	fi
 fi
 
 #haproxy
