ea1d1325d8ad99cb328a6a368e1df5eac2455796
Lienol <lawlienol@gmail.com>
2019-11-14 17:30:48 +0800

    passwall : update and fix
    Fixed an issue with v2ray server not being able to save .
    Optimize dns2socks forwarding scheme .

--- lienol/luci-app-passwall/luasrc/model/cbi/passwall/global.lua
+++ lienol/luci-app-passwall/luasrc/model/cbi/passwall/global.lua
@@ -81,13 +81,14 @@ if has_udp_relay() then
 end
 
 ---- Socks5 Proxy Server
-o = s:option(ListValue, "socks5_proxy_server1", translate("Socks5 Proxy Server"),
+o = s:option(ListValue, "socks5_proxy_server1",
+             translate("Socks5 Proxy Server"),
              translate("The client can use the router's Socks5 proxy"))
 o:value("nil", translate("Close"))
 for _, key in pairs(key_table) do o:value(key, n[key]) end
 
 local socks5_proxy_server_num = uci:get(appname, "@global_other[0]",
-                                     "socks5_proxy_server_num")
+                                        "socks5_proxy_server_num")
 if socks5_proxy_server_num and tonumber(socks5_proxy_server_num) >= 2 then
     for i = 2, socks5_proxy_server_num, 1 do
         o = s:option(ListValue, "socks5_proxy_server" .. i,
@@ -106,8 +107,7 @@ if is_installed("ChinaDNS") or is_finded("chinadns") then
 end
 if (is_installed("dns2socks") or is_finded("dns2socks")) and
     (is_finded("ss-local") or is_finded("ssr-local")) then
-    o:value("dns2socks",
-            "dns2socks " .. translate("Only SS/R servers are supported"))
+    o:value("dns2socks", "dns2socks " .. translate("Need Socks5 server"))
 end
 if is_installed("pcap-dnsproxy") or is_finded("Pcap_DNSProxy") then
     o:value("Pcap_DNSProxy", "Pcap_DNSProxy")
