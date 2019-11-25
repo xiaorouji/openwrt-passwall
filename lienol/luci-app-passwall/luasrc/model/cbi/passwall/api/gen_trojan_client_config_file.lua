c9e4b1de38a609ea037ab347c646682569c2b679
bubbling <rex@outlook.tv>
2019-11-25 04:18:29 -0800

    passwal: add  trojan certificate  related option to fix certificate  verify issue

--- lienol/luci-app-passwall/luasrc/model/cbi/passwall/api/gen_trojan_client_config_file.lua
+++ lienol/luci-app-passwall/luasrc/model/cbi/passwall/api/gen_trojan_client_config_file.lua
@@ -14,9 +14,9 @@ local trojan = {
     password = {server.password},
     log_level = 1,
     ssl = {
-        verify = true,
+        verify = (server.trojan_verify_cert == "1") and true or false,
         verify_hostname = true,
-        cert = "",
+        cert = server.trojan_cert_path,
         cipher = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:RSA-AES128-GCM-SHA256:RSA-AES256-GCM-SHA384:RSA-AES128-SHA:RSA-AES256-SHA:RSA-3DES-EDE-SHA",
         sni = "",
         alpn = {"h2", "http/1.1"},
