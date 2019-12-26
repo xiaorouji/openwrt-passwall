642e696fd8247afba36355fb31beaf44e25e8240
Lienol <lawlienol@gmail.com>
2019-12-27 05:30:30 +0800

    tidy source code

--- lienol/luci-app-guest-wifi/luasrc/controller/guest-wifi.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.guest-wifi", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/guest-wifi") then
-		return
-	end
-	entry({"admin","network","guest-wifi"}, cbi("guest-wifi"), translate("Guest-wifi"), 16)
-end
