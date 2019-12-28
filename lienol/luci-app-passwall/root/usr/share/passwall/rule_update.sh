0520c1b626c8c2bbce9df910533374af7751c369
Lienol <lawlienol@gmail.com>
2019-12-28 19:42:00 +0800

    passwall : enhanced chinadns-ng mode

--- lienol/luci-app-passwall/root/usr/share/passwall/rule_update.sh
+++ lienol/luci-app-passwall/root/usr/share/passwall/rule_update.sh
@@ -13,12 +13,14 @@ chnroute_update=0
 if [ -n "$update" ]; then
 	[ -n "$(echo $update | grep "gfwlist_update")" ] && gfwlist_update=1
 	[ -n "$(echo $update | grep "chnroute_update")" ] && chnroute_update=1
+	[ -n "$(echo $update | grep "chnlist_update")" ] && chnlist_update=1
 else
 	gfwlist_update=$(uci get $CONFIG.@global_rules[0].gfwlist_update)
 	chnroute_update=$(uci get $CONFIG.@global_rules[0].chnroute_update)
+	chnlist_update=$(uci get $CONFIG.@global_rules[0].chnlist_update)
 fi
 
-if [ "$gfwlist_update" == 0 -a "$chnroute_update" == 0 ]; then
+if [ "$gfwlist_update" == 0 -a "$chnroute_update" == 0 -a "$chnlist_update" == 0 ]; then
 	exit
 fi
 
@@ -127,13 +129,52 @@ if [ "$chnroute_update" == 1 ]; then
 	rm -rf /tmp/chnroute
 fi
 
+# update chnlist
+if [ "$chnlist_update" == 1 ]; then
+	chnlist=$(cat /tmp/version1 | sed -n 4p)
+	version_chnlist2=$(echo $chnlist | sed 's/ /\n/g' | sed -n 1p)
+	md5sum_chnlist2=$(echo $chnlist | sed 's/ /\n/g' | tail -n 2 | head -n 1)
+	local_md5sum_chnlist=$(md5sum $RULE_PATH/chnlist | awk '{print $1}')
+	if [ ! -z "$version_chnlist2" ]; then
+		version_chnlist1=$(uci_get_by_type global_rules chnlist_version)
+		if [ "$version_chnlist1" != "$version_chnlist2" -o "$md5sum_chnlist2" != "$local_md5sum_chnlist" ]; then
+			echo $Date: 检测到新版本chnlist，开始更新... >>$LOG_FILE
+			echo $Date: 下载chnlist到临时文件... >>$LOG_FILE
+			status=$(/usr/bin/curl -w %{http_code} --connect-timeout 10 $url_main/cdn.txt --silent -o /tmp/chnlist)
+			[ "$?" != 0 ] || [ -z "$status" ] || [ "$status" != "200" ] && {
+				status=$(/usr/bin/wget --no-check-certificate --timeout=15 -q $url_main/cdn.txt -O /tmp/chnlist)
+				[ "$?" != 0 ] || [ -z "$status" ] && {
+					echo $Date: 无法访问更新接口，请更新接口！ >>$LOG_FILE
+					exit
+				}
+			}
+			md5sum_chnlist1=$(md5sum /tmp/chnlist | sed 's/ /\n/g' | sed -n 1p)
+			if [ "$md5sum_chnlist1"x = "$md5sum_chnlist2"x ]; then
+				echo $Date: 下载完成，校验通过，将临时文件覆盖到原始chnlist文件 >>$LOG_FILE
+				mv /tmp/chnlist $RULE_PATH/chnlist
+				uci set $CONFIG.@global_rules[0].chnlist_version=$version_chnlist2
+				reboot="1"
+				echo $Date: 你的chnlist已经更新到最新了哦~ >>$LOG_FILE
+			else
+				echo $Date: 下载完成，但是校验没有通过！ >>$LOG_FILE
+			fi
+		else
+			echo $Date: 检测到chnlist本地版本号和在线版本号相同，不用更新! >>$LOG_FILE
+		fi
+	else
+		echo $Date: chnlist文件下载失败！ >>$LOG_FILE
+	fi
+	rm -rf /tmp/chnlist
+fi
+
 echo $Date: 更新进程运行完毕！ >>$LOG_FILE
 rm -rf /tmp/version1
 # write number
 uci set $CONFIG.@global_rules[0].gfwlist_update=$gfwlist_update
 uci set $CONFIG.@global_rules[0].chnroute_update=$chnroute_update
+uci set $CONFIG.@global_rules[0].chnlist_update=$chnlist_update
 uci commit
-# reboot ss
+# reboot
 if [ "$reboot" == "1" ]; then
 	echo $Date: 重启软件，以应用新的规则文件！请稍后！ >>$LOG_FILE
 	/etc/init.d/$CONFIG restart
