fa22199689a08a36e1e153da451c16e1332c784b
peter-tank <30540412+peter-tank@users.noreply.github.com>
2020-01-04 02:21:32 +0800

    proper encloser for inputer & break line

--- lienol/luci-app-passwall/luasrc/model/cbi/passwall/node_config.lua
+++ lienol/luci-app-passwall/luasrc/model/cbi/passwall/node_config.lua
@@ -239,7 +239,7 @@ v2ray_tcp_guise_http_path:depends("v2ray_tcp_guise", "http")
 
 v2ray_mkcp_guise = s:option(ListValue, "v2ray_mkcp_guise",
                             translate("Camouflage Type"), translate(
-                                '<br>none: default, no masquerade, data sent is packets with no characteristics.<br>srtp: disguised as an SRTP packet, it will be recognized as video call data (such as FaceTime).<br>utp: packets disguised as uTP will be recognized as bittorrent downloaded data.<br>wechat-video: packets disguised as WeChat video calls.<br>dtls: disguised as DTLS 1.2 packet.<br>wireguard: disguised as a WireGuard packet. (not really WireGuard protocol)'))
+                                '<br />none: default, no masquerade, data sent is packets with no characteristics.<br />srtp: disguised as an SRTP packet, it will be recognized as video call data (such as FaceTime).<br />utp: packets disguised as uTP will be recognized as bittorrent downloaded data.<br />wechat-video: packets disguised as WeChat video calls.<br />dtls: disguised as DTLS 1.2 packet.<br />wireguard: disguised as a WireGuard packet. (not really WireGuard protocol)'))
 for a, t in ipairs(v2ray_header_type_list) do v2ray_mkcp_guise:value(t) end
 v2ray_mkcp_guise:depends("v2ray_transport", "mkcp")
 
