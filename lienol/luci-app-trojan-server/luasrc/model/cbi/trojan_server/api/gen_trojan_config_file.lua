a5eb5571d755bd4f380b8b2f3fd3890d4f4df01b
peter-tank <30540412+peter-tank@users.noreply.github.com>
2020-01-15 10:45:21 +0800

    luci-app-trojan-server: update ciphers and disable reuse_port in default
    Signed-off-by: peter-tank <30540412+peter-tank@users.noreply.github.com>

--- lienol/luci-app-trojan-server/luasrc/model/cbi/trojan_server/api/gen_trojan_config_file.lua
+++ lienol/luci-app-trojan-server/luasrc/model/cbi/trojan_server/api/gen_trojan_config_file.lua
@@ -17,7 +17,8 @@ local trojan = {
         cert = server.ssl_certFile,
         key = server.ssl_keyFile,
         key_password = "",
-        cipher = "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256",
+        cipher = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA:AES128-SHA:AES256-SHA:DES-CBC3-SHA",
+        cipher_tls13 = "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
         prefer_server_cipher = true,
         alpn = {"http/1.1"},
         reuse_session = true,
@@ -31,6 +32,7 @@ local trojan = {
         prefer_ipv4 = false,
         no_delay = true,
         keep_alive = true,
+        reuse_port = false,
         fast_open = (server.fast_open == "true") and true or false,
         fast_open_qlen = 20
     }
