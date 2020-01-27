c651c473a7a25e32ece984ca1b476ac707920bf2
xiaorouji <>
2020-01-28 02:27:10 +0800

    passwall : fix kcptun can't run bug

--- lienol/luci-app-passwall/luasrc/model/cbi/passwall/global.lua
+++ lienol/luci-app-passwall/luasrc/model/cbi/passwall/global.lua
@@ -148,13 +148,14 @@ o:value("208.67.222.222,208.67.220.220",
 o:depends("dns_mode", "chinadns-ng")
 
 ---- Use TCP Node Resolve DNS
-if is_installed("pdnsd") or is_installed("pdnsd-alt") or is_finded("pdnsd") then
+--[[ if is_installed("pdnsd") or is_installed("pdnsd-alt") or is_finded("pdnsd") then
     o = s:option(Flag, "use_tcp_node_resolve_dns",
                  translate("Use TCP Node Resolve DNS"),
                  translate("If checked, DNS is resolved using the TCP node."))
     o.default = 1
     o:depends("dns_mode", "pdnsd")
 end
+--]]
 
 o = s:option(Value, "dns2socks_forward", translate("DNS Address"))
 o.default = "8.8.4.4"
