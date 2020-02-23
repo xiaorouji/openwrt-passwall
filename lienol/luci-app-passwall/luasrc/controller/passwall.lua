cdf34dd12325b906fcc84f66f31057fd60cc050b
ShanStone <31815718+ShanStone@users.noreply.github.com>
2020-02-23 15:31:26 +0800

    fix uhttpd cbi() error log (#283)
    daemon.err uhttpd[2381]: Model passwall/log returns SimpleForm but is dispatched via cbi()

--- lienol/luci-app-passwall/luasrc/controller/passwall.lua
+++ lienol/luci-app-passwall/luasrc/controller/passwall.lua
@@ -39,7 +39,7 @@ function index()
     entry({"admin", "vpn", "passwall", "rule_list"},
           cbi("passwall/rule_list", {autoapply = true}),
           _("Set Blacklist And Whitelist"), 98).leaf = true
-    entry({"admin", "vpn", "passwall", "log"}, cbi("passwall/log"),
+    entry({"admin", "vpn", "passwall", "log"}, form("passwall/log"),
           _("Watch Logs"), 99).leaf = true
     entry({"admin", "vpn", "passwall", "node_config"},
           cbi("passwall/node_config")).leaf = true
