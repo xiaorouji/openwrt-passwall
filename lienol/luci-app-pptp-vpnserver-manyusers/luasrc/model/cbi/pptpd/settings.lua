6d0bc47978857b8b37b93ca74fd32678acfe73e8
Lienol <lawlienol@gmail.com>
2020-03-02 01:20:48 +0800

    luci-app-pptp-*: simplify

--- lienol/luci-app-pptp-vpnserver-manyusers/luasrc/model/cbi/pptpd/settings.lua
+++ lienol/luci-app-pptp-vpnserver-manyusers/luasrc/model/cbi/pptpd/settings.lua
@@ -30,32 +30,16 @@ o.placeholder = translate("192.168.1.10-20")
 o.rmempty = true
 o.default = "192.168.1.10-20"
 
+--[[
 o = s:option(Value, "dns", translate("DNS IP address"),
              translate("This will be sent to the client, it not required."))
 o.placeholder = translate("192.168.1.1")
 o.datatype = "ipaddr"
 o.rmempty = true
 o.default = "192.168.1.1"
+]]--
 
 o = s:option(Flag, "mppe", translate("Enable MPPE Encryption"),
              translate("Allows 128-bit encrypted connection."))
 o.rmempty = false
-
-o = s:option(Flag, "is_nat", translate("is_nat"))
-o.rmempty = false
-
-o = s:option(ListValue, "export_interface", translate("Interface"),
-             translate("Specify interface forwarding traffic."))
-o:value("default", translate("default"))
-for _, iface in ipairs(ifaces) do
-    if (iface:match("^br*") or iface:match("^eth*") or iface:match("^pppoe*") or
-        iface:match("wlan*")) then
-        local nets = net:get_interface(iface)
-        nets = nets and nets:get_networks() or {}
-        for k, v in pairs(nets) do nets[k] = nets[k].sid end
-        nets = table.concat(nets, ",")
-        o:value(iface, ((#nets > 0) and "%s (%s)" % {iface, nets} or iface))
-    end
-end
-o:depends("is_nat", "1")
 return m
