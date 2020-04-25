655647a48067d869d43c3ee31a926984664514a0
Lienol <lawlienol@gmail.com>
2020-04-26 00:43:19 +0800

    ssr-mudb-server: update

---
+++ lienol/luci-app-ssr-mudb-server/root/usr/share/ssr_mudb_server/firewall.lua
@@ -0,0 +1,15 @@
+#!/usr/bin/lua
+
+require 'luci.sys'
+local luci = luci
+local jsonc = require "luci.jsonc"
+
+local json = jsonc.parse(luci.sys.exec("cat /usr/share/ssr_mudb_server/mudb.json"))
+if json then
+	for index = 1, table.maxn(json) do
+		local o = json[index]
+		if o.enable == 1 then
+			luci.sys.call(string.format("iptables -A SSR_MUDB-SERVER -p tcp --dport %s -m comment --comment %s -j ACCEPT", o.port, o.user))
+		end
+	end
+end
