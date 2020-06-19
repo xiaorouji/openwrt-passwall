00cf340e32c39a89fb59d307aa7e2c75707251e7
Lienol <lawlienol@gmail.com>
2020-06-19 23:50:22 +0800

    luci-app-ssr-mudb-server: fix https://github.com/Lienol/openwrt-package/issues/455

--- lienol/luci-app-ssr-mudb-server/root/usr/share/ssr_mudb_server/firewall.lua
+++ lienol/luci-app-ssr-mudb-server/root/usr/share/ssr_mudb_server/firewall.lua
@@ -1,10 +1,17 @@
 #!/usr/bin/lua
 
 require 'luci.sys'
-local luci = luci
 local jsonc = require "luci.jsonc"
 
-local json = jsonc.parse(luci.sys.exec("cat /usr/share/ssr_mudb_server/mudb.json"))
+local function get_config_path()
+    return luci.sys.exec("echo -n $(cat /usr/share/ssr_mudb_server/userapiconfig.py | grep 'MUDB_FILE' | cut -d \"'\" -f 2)")
+end
+
+local function get_config_json()
+    return luci.sys.exec("cat " .. get_config_path()) or "[]"
+end
+
+local json = jsonc.parse(get_config_json())
 if json then
 	for index = 1, table.maxn(json) do
 		local o = json[index]
