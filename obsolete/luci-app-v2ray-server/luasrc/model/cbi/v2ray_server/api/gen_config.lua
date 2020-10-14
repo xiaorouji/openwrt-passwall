ce8ded7f48cab1866ca7768d32ea8b46b122c7a7
xiaorouji <60100640+xiaorouji@users.noreply.github.com>
2020-10-14 17:10:53 +0800

    v2ray_server: sync to passwall v2ray server

--- obsolete/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/api/gen_config.lua
+++ obsolete/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/api/gen_config.lua
@@ -1,223 +1,184 @@
-local ucursor = require"luci.model.uci".cursor()
-local json = require "luci.jsonc"
-local server_section = arg[1]
-local server = ucursor:get_all("v2ray_server", server_section)
+module("luci.model.cbi.v2ray_server.api.gen_config", package.seeall)
 
-local settings = nil
-local routing = nil
-local outbounds = {
-    {protocol = "freedom"},
-    {protocol = "blackhole", tag = "blocked"}
-}
+function gen_config(user)
+    local settings = nil
+    local routing = nil
+    local outbounds = {
+        {protocol = "freedom", tag = "direct"}, {protocol = "blackhole", tag = "blocked"}
+    }
 
-if server.protocol == "vmess" then
-    if server.vmess_id then
-        local clients = {}
-        for i = 1, #server.vmess_id do
-            clients[i] = {
-                id = server.vmess_id[i],
-                level = tonumber(server.vmess_level),
-                alterId = tonumber(server.alter_id)
+    if user.protocol == "vmess" or user.protocol == "vless" then
+        if user.uuid then
+            local clients = {}
+            for i = 1, #user.uuid do
+                clients[i] = {
+                    id = user.uuid[i],
+                    flow = (user.xtls and user.xtls == "1") and user.flow or nil,
+                    level = tonumber(user.level),
+                    alterId = tonumber(user.alter_id)
+                }
+            end
+            settings = {
+                clients = clients,
+                decryption = user.decryption or "none"
             }
         end
-        settings = {clients = clients}
+    elseif user.protocol == "socks" then
+        settings = {
+            auth = (user.auth and user.auth == "1") and "password" or "noauth",
+            accounts = (user.auth and user.auth == "1") and {
+                {
+                    user = user.username,
+                    pass = user.password
+                }
+            }
+        }
+    elseif user.protocol == "http" then
+        settings = {
+            allowTransparent = false,
+            accounts = (user.auth and user.auth == "1") and {
+                {
+                    user = user.username,
+                    pass = user.password
+                }
+            }
+        }
+        user.transport = "tcp"
+        user.tcp_guise = "none"
+    elseif user.protocol == "shadowsocks" then
+        settings = {
+            method = user.method,
+            password = user.password,
+            level = tonumber(user.level) or 1,
+            network = user.ss_network or "TCP,UDP"
+        }
+    elseif user.protocol == "trojan" then
+        if user.uuid then
+            local clients = {}
+            for i = 1, #user.uuid do
+                clients[i] = {
+                    password = user.uuid[i],
+                    level = tonumber(user.level)
+                }
+            end
+            settings = {
+                clients = clients
+            }
+        end
+    elseif user.protocol == "mtproto" then
+        settings = {
+            users = {
+                {
+                    level = tonumber(user.level) or 1,
+                    secret = (user.password == nil) and "" or user.password
+                }
+            }
+        }
     end
-elseif server.protocol == "socks" then
-    settings = {
-        auth = (server.username == nil and server.password == nil) and "noauth" or "password",
-        accounts = {
-            {
-                user = (server.username == nil) and "" or server.username,
-                pass = (server.password == nil) and "" or server.password
-            }
-        }
-    }
-elseif server.protocol == "http" then
-    settings = {
-        allowTransparent = false,
-        accounts = {
-            {
-                user = (server.username == nil) and "" or server.username,
-                pass = (server.password == nil) and "" or server.password
-            }
-        }
-    }
-elseif server.protocol == "shadowsocks" then
-    settings = {
-        method = server.ss_method,
-        password = server.password,
-        level = tonumber(server.ss_level),
-        network = server.ss_network,
-        ota = (server.ss_ota == '1') and true or false
-    }
-end
 
-if server.accept_lan == nil or server.accept_lan == "0" then
     routing = {
         domainStrategy = "IPOnDemand",
         rules = {
             {
                 type = "field",
                 ip = {"10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"},
-                outboundTag = "blocked"
+                outboundTag = (user.accept_lan == nil or user.accept_lan == "0") and "blocked" or "direct"
             }
         }
     }
-end
 
-if server.transit_node and server.transit_node ~= "nil" then
-    local node = ucursor:get_all("passwall", server.transit_node)
-    if node and node ~= "nil" and node.type and node.type == "V2ray" then
-        local transit_node = {
-            tag = "transit",
-            protocol = node.protocol or "vmess",
-            mux = {
-                enabled = (node.mux == "1") and true or false,
-                concurrency = (node.mux_concurrency) and tonumber(node.mux_concurrency) or 8
-            },
-            -- 底层传输配置
-            streamSettings = (node.protocol == "vmess") and {
-                network = node.transport,
-                security = node.stream_security,
-                tlsSettings = (node.stream_security == "tls") and {
-                    serverName = node.tls_serverName,
-                    allowInsecure = (node.tls_allowInsecure == "1") and true or false
-                } or nil,
-                tcpSettings = (node.transport == "tcp") and {
-                    header = {
-                        type = node.tcp_guise,
-                        request = {
-                            path = node.tcp_guise_http_path or {"/"},
-                            headers = {
-                                Host = node.tcp_guise_http_host or {}
-                            }
-                        } or {}
-                    }
-                } or nil,
-                kcpSettings = (node.transport == "mkcp") and {
-                    mtu = tonumber(node.mkcp_mtu),
-                    tti = tonumber(node.mkcp_tti),
-                    uplinkCapacity = tonumber(node.mkcp_uplinkCapacity),
-                    downlinkCapacity = tonumber(node.mkcp_downlinkCapacity),
-                    congestion = (node.mkcp_congestion == "1") and true or false,
-                    readBufferSize = tonumber(node.mkcp_readBufferSize),
-                    writeBufferSize = tonumber(node.mkcp_writeBufferSize),
-                    header = {type = node.mkcp_guise}
-                } or nil,
-                wsSettings = (node.transport == "ws") and {
-                    path = node.ws_path or "",
-                    headers = (node.ws_host ~= nil) and {Host = node.ws_host} or nil
-                } or nil,
-                httpSettings = (node.transport == "h2") and {
-                    path = node.h2_path, host = node.h2_host
-                } or nil,
-                dsSettings = (node.transport == "ds") and {
-                    path = node.ds_path
-                } or nil,
-                quicSettings = (node.transport == "quic") and {
-                    security = node.quic_security,
-                    key = node.quic_key,
-                    header = {type = node.quic_guise}
-                } or nil
-            } or nil,
-            settings = {
-                vnext = (node.protocol == "vmess") and {
-                    {
-                        address = node.address,
-                        port = tonumber(node.port),
-                        users = {
+    local config = {
+        log = {
+            -- error = "/var/etc/v2ray_server/log/" .. user[".name"] .. ".log",
+            loglevel = "warning"
+        },
+        -- 传入连接
+        inbounds = {
+            {
+                listen = (user.bind_local == "1") and "127.0.0.1" or nil,
+                port = tonumber(user.port),
+                protocol = user.protocol,
+                settings = settings,
+                streamSettings = {
+                    network = user.transport,
+                    security = "none",
+                    xtlsSettings = (user.tls and user.tls == "1" and user.xtls and user.xtls == "1") and {
+                        --alpn = {"http/1.1"},
+                        disableSystemRoot = false,
+                        certificates = {
                             {
-                                id = node.vmess_id,
-                                alterId = tonumber(node.alter_id),
-                                level = tonumber(node.vmess_level),
-                                security = node.security
+                                certificateFile = user.tls_certificateFile,
+                                keyFile = user.tls_keyFile
                             }
                         }
-                    }
-                } or nil,
-                servers = (node.protocol == "http" or node.protocol == "socks" or node.protocol == "shadowsocks") and {
-                    {
-                        address = node.address,
-                        port = tonumber(node.port),
-                        method = node.ss_encrypt_method,
-                        password = node.password or "",
-                        ota = (node.ss_ota == '1') and true or false,
-                        users = (node.username and node.password) and {
+                    } or nil,
+                    tlsSettings = (user.tls and user.tls == "1") and {
+                        disableSystemRoot = false,
+                        certificates = {
                             {
-                                user = node.username or "",
-                                pass = node.password or ""
+                                certificateFile = user.tls_certificateFile,
+                                keyFile = user.tls_keyFile
                             }
-                        } or nil
-                    }
-                } or nil
+                        }
+                    } or nil,
+                    tcpSettings = (user.transport == "tcp") and {
+                        header = {
+                            type = user.tcp_guise,
+                            request = (user.tcp_guise == "http") and {
+                                path = user.tcp_guise_http_path or {"/"},
+                                headers = {
+                                    Host = user.tcp_guise_http_host or {}
+                                }
+                            } or nil
+                        }
+                    } or nil,
+                    kcpSettings = (user.transport == "mkcp") and {
+                        mtu = tonumber(user.mkcp_mtu),
+                        tti = tonumber(user.mkcp_tti),
+                        uplinkCapacity = tonumber(user.mkcp_uplinkCapacity),
+                        downlinkCapacity = tonumber(user.mkcp_downlinkCapacity),
+                        congestion = (user.mkcp_congestion == "1") and true or false,
+                        readBufferSize = tonumber(user.mkcp_readBufferSize),
+                        writeBufferSize = tonumber(user.mkcp_writeBufferSize),
+                        seed = (user.mkcp_seed and user.mkcp_seed ~= "") and user.mkcp_seed or nil,
+                        header = {type = user.mkcp_guise}
+                    } or nil,
+                    wsSettings = (user.transport == "ws") and {
+                        acceptProxyProtocol = false,
+                        headers = (user.ws_host) and {Host = user.ws_host} or nil,
+                        path = user.ws_path
+                    } or nil,
+                    httpSettings = (user.transport == "h2") and {
+                        path = user.h2_path, host = user.h2_host
+                    } or nil,
+                    dsSettings = (user.transport == "ds") and {
+                        path = user.ds_path
+                    } or nil,
+                    quicSettings = (user.transport == "quic") and {
+                        security = user.quic_security,
+                        key = user.quic_key,
+                        header = {type = user.quic_guise}
+                    } or nil
+                }
             }
-        }
-        table.insert(outbounds, 1, transit_node)
-    end
-end
+        },
+        -- 传出连接
+        outbounds = outbounds,
+        routing = routing
+    }
 
-local v2ray = {
-    log = {
-        --error = string.format("/var/log/v2ray_%s.log", server[".name"]),
-        loglevel = "warning"
-    },
-    -- 传入连接
-    inbounds = {{
-        listen = (server.bind_local == "1") and "127.0.0.1" or nil,
-        port = tonumber(server.port),
-        protocol = server.protocol,
-        -- 底层传输配置
-        settings = settings,
-        streamSettings = (server.protocol == "vmess") and {
-            network = server.transport,
-            security = (server.tls_enable == '1') and "tls" or "none",
-            tlsSettings = (server.tls_enable == '1') and {
-                -- serverName = (server.tls_serverName),
-                allowInsecure = false,
-                disableSystemRoot = false,
-                certificates = {
-                    {
-                        certificateFile = server.tls_certificateFile,
-                        keyFile = server.tls_keyFile
-                    }
-                }
-            } or nil,
-            tcpSettings = (server.transport == "tcp") and {
-                header = {
-                    type = server.tcp_guise,
-                    request = {
-                        path = server.tcp_guise_http_path or {"/"},
-                        headers = {
-                            Host = server.tcp_guise_http_host or {}
-                        }
-                    } or {}
-                }
-            } or nil,
-            kcpSettings = (server.transport == "mkcp") and {
-                mtu = tonumber(server.mkcp_mtu),
-                tti = tonumber(server.mkcp_tti),
-                uplinkCapacity = tonumber(server.mkcp_uplinkCapacity),
-                downlinkCapacity = tonumber(server.mkcp_downlinkCapacity),
-                congestion = (server.mkcp_congestion == "1") and true or false,
-                readBufferSize = tonumber(server.mkcp_readBufferSize),
-                writeBufferSize = tonumber(server.mkcp_writeBufferSize),
-                header = {type = server.mkcp_guise}
-            } or nil,
-            wsSettings = (server.transport == "ws") and {
-                headers = (server.ws_host) and {Host = server.ws_host} or nil,
-                path = server.ws_path
-            } or nil,
-            httpSettings = (server.transport == "h2") and
-                {path = server.h2_path, host = server.h2_host} or nil,
-            quicSettings = (server.transport == "quic") and {
-                security = server.quic_security,
-                key = server.quic_key,
-                header = {type = server.quic_guise}
-            } or nil
-        } or nil
-    }},
-    -- 传出连接
-    outbounds = outbounds,
-    routing = routing
-}
-print(json.stringify(v2ray, 1))
+    if user.tls and user.tls == "1" then
+        config.inbounds[1].streamSettings.security = "tls"
+        if user.xtls and user.xtls == "1" then
+            config.inbounds[1].streamSettings.security = "xtls"
+            config.inbounds[1].streamSettings.tlsSettings = nil
+        end
+    end
+
+    if user.transport == "mkcp" or user.transport == "quic" then
+        config.inbounds[1].streamSettings.security = "none"
+        config.inbounds[1].streamSettings.tlsSettings = nil
+    end
+
+    return config
+end
