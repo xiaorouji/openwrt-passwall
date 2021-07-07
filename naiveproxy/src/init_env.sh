0fefb8073989e3d595118f570293e844cdca4809
Beginner <70857188+Beginner-Go@users.noreply.github.com>
2021-07-07 13:16:54 +0800

    naiveproxy: sync upstream
    * naiveproxy: tweak script
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    
    * naiveproxy: refresh patches
    
    Co-authored-by: Tianling Shen <cnsztl@immortalwrt.org>
    Co-authored-by: zxlhhyccc <45259624+zxlhhyccc@users.noreply.github.com>

--- naiveproxy/src/init_env.sh
+++ naiveproxy/src/init_env.sh
@@ -7,17 +7,25 @@
 
 # Read args from shell
 target_arch="$1"
-target_board="$2"
-cpu_type="$3"
-cpu_subtype="$4"
-toolchain_dir="$5"
+cpu_type="$2"
+cpu_subtype="$3"
+toolchain_dir="$4"
 
 # Set arch info
-naive_arch="${target_arch}"
-[ "${target_arch}" == "i386" ] && naive_arch="x86"
-[ "${target_arch}" == "x86_64" ] && naive_arch="x64"
-[ "${target_arch}" == "aarch64" ] && naive_arch="arm64"
-# ldso_path="/lib/$(find "${toolchain_dir}/" | grep -Eo "ld-musl-[a-z0-9_-]+\\.so\\.1")"
+case "${target_arch}" in
+"aarch64")
+	naive_arch="arm64"
+	;;
+"i386")
+	naive_arch="x86"
+	;;
+"x86_64")
+	naive_arch="x64"
+	;;
+*)
+	naive_arch="${target_arch}"
+	;;
+esac
 
 # OS detection
 [ "$(uname)" != "Linux" -o "$(uname -m)" != "x86_64" ] && { echo -e "Support Linux AMD64 only."; exit 1; }
@@ -62,17 +70,23 @@ include_transport_security_state_preload_list=false
 target_os=\"openwrt\"
 target_cpu=\"${naive_arch}\"
 target_sysroot=\"${toolchain_dir}\""
-# ldso_path=\"${ldso_path}\""
-[ "${target_arch}" == "arm" ] && {
-	naive_flags="${naive_flags} arm_version=0 arm_cpu=\"${cpu_type}\""
+
+case "${target_arch}" in
+"arm")
+	naive_flags+=" arm_version=0 arm_cpu=\"${cpu_type}\""
 	if [ -n "${cpu_subtype}" ]; then
-		echo "${cpu_subtype}" | grep -q "neon" && neon_flag="arm_use_neon=true" || neon_flag="arm_use_neon=false"
-		naive_flags="${naive_flags} arm_fpu=\"${cpu_subtype}\" arm_float_abi=\"hard\" ${neon_flag}"
+		if grep -q "neon" <<< "${cpu_subtype}"; then
+			neon_flag="arm_use_neon=true"
+		else
+			neon_flag="arm_use_neon=false"
+		fi
+		naive_flags+=" arm_fpu=\"${cpu_subtype}\" arm_float_abi=\"hard\" ${neon_flag}"
 	else
-		naive_flags="${naive_flags} arm_float_abi=\"soft\" arm_use_neon=false"
+		naive_flags+=" arm_float_abi=\"soft\" arm_use_neon=false"
 	fi
-}
-[[ "mips mips64 mipsel mips64el" =~ (^|[[:space:]])"${target_arch}"($|[[:space:]]) ]] && {
-	naive_flags="${naive_flags} use_gold=false is_cfi=false use_cfi_icall=false use_thin_lto=false mips_arch_variant=\"r2\""
-	[[ "${target_arch}" =~ ^"mips"$|^"mipsel"$ ]] && naive_flags="${naive_flags} mips_float_abi=\"soft\" mips_tune=\"${cpu_type}\""
-}
+	;;
+"mips"|"mips64"|"mipsel"|"mips64el")
+	naive_flags+=" use_gold=false is_cfi=false use_cfi_icall=false use_thin_lto=false mips_arch_variant=\"r2\""
+	[[ "${target_arch}" =~ "mips"|"mipsel" ]] && naive_flags+=" mips_float_abi=\"soft\" mips_tune=\"${cpu_type}\""
+	;;
+esac
