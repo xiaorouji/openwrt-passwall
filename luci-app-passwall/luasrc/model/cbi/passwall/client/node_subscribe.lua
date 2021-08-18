f447b0c3e1d32f5966badc1dfbb8825eb3dc6c98
xiaorouji <60100640+xiaorouji@users.noreply.github.com>
2021-08-18 16:43:15 +0800

    luci: 4.34
    luci: optimization code
    
    luci: optimize subscription logic, normal sort
    
    luci: optimization ss-rust support

--- luci-app-passwall/luasrc/model/cbi/passwall/client/node_subscribe.lua
+++ luci-app-passwall/luasrc/model/cbi/passwall/client/node_subscribe.lua
@@ -1,18 +1,29 @@
 local api = require "luci.model.cbi.passwall.api.api"
 local appname = api.appname
+local has_ss = api.is_finded("ss-redir")
+local has_ss_rust = api.is_finded("sslocal")
 local has_trojan_plus = api.is_finded("trojan-plus")
 local has_v2ray = api.is_finded("v2ray")
 local has_xray = api.is_finded("xray")
 local has_trojan_go = api.is_finded("trojan-go")
+local ss_aead_type = {}
 local trojan_type = {}
+if has_ss then
+    ss_aead_type[#ss_aead_type + 1] = "shadowsocks-libev"
+end
+if has_ss_rust then
+    ss_aead_type[#ss_aead_type + 1] = "shadowsocks-rust"
+end
 if has_trojan_plus then
     trojan_type[#trojan_type + 1] = "trojan-plus"
 end
 if has_v2ray then
     trojan_type[#trojan_type + 1] = "v2ray"
+    ss_aead_type[#ss_aead_type + 1] = "v2ray"
 end
 if has_xray then
     trojan_type[#trojan_type + 1] = "xray"
+    ss_aead_type[#ss_aead_type + 1] = "xray"
 end
 if has_trojan_go then
     trojan_type[#trojan_type + 1] = "trojan-go"
@@ -61,6 +72,13 @@ o = s:option(DynamicList, "filter_keep_list", translate("Keep List"))
 o = s:option(Flag, "allowInsecure", translate("allowInsecure"), translate("Whether unsafe connections are allowed. When checked, Certificate validation will be skipped."))
 o.default = "1"
 o.rmempty = false
+
+if #ss_aead_type > 0 then
+    o = s:option(ListValue, "ss_aead_type", translate("SS AEAD Node Use Type"))
+    for key, value in pairs(ss_aead_type) do
+        o:value(value, translate(value:gsub("^%l",string.upper)))
+    end
+end
 
 if #trojan_type > 0 then
     o = s:option(ListValue, "trojan_type", translate("Trojan Node Use Type"))
