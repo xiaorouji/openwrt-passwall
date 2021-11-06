5e461faff381ca56e776e5aefae02792150d41b2
zhusir <smallprogram@foxmail.com>
2021-11-06 20:19:04 +0800

    update passwall to 4.43

--- luci-app-passwall/root/usr/share/passwall/iptables.sh
+++ luci-app-passwall/root/usr/share/passwall/iptables.sh
@@ -142,35 +142,11 @@ get_action_chain_name() {
 }
 
 gen_laniplist() {
-	cat <<-EOF
-		0.0.0.0/8
-		10.0.0.0/8
-		100.64.0.0/10
-		127.0.0.0/8
-		169.254.0.0/16
-		172.16.0.0/12
-		192.168.0.0/16
-		224.0.0.0/4
-		240.0.0.0/4
-	EOF
+	cat /usr/share/passwall/rules/lanlist_ipv4
 }
 
 gen_laniplist_6() {
-	cat <<-EOF
-		::/128
-		::1/128
-		::ffff:0:0/96
-		::ffff:0:0:0/96
-		64:ff9b::/96
-		100::/64
-		2001::/32
-		2001:20::/28
-		2001:db8::/32
-		2002::/16
-		fc00::/7
-		fe80::/10
-		ff00::/8
-	EOF
+	cat /usr/share/passwall/rules/lanlist_ipv4
 }
 
 load_acl() {
