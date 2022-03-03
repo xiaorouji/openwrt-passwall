c6c1bc39b4fddada2576d37d2268ce52fcc99c00
xiaorouji <60100640+xiaorouji@users.noreply.github.com>
2022-03-04 00:31:12 +0800

    luci: 4.39
    luci: add some options
    
    * add Websocket maxEarlyData option
    * add domainMatcher option
    * add some CN translates
    
    luci: Add health check for h2 & gRPC
    
    luci: Add port 853(DOT) to common use TCP port
    
    luci: Add Steam and Epic platforms directly Host
    
    Increase the direct connection of Steam and Epic platforms to Host to prevent proxy traffic when updating or downloading games
    
    luci: Handle empty sni for vmess and vless
    Co-Authored-By: MoetaYuko <loli@yuko.moe>
    Co-Authored-By: zhusir <smallprogram@foxmail.com>
    Co-Authored-By: Gzxhwq <8738520+Gzxhwq@users.noreply.github.com>
    Co-Authored-By: owmyfeellings <46664129+owmyfeellings@users.noreply.github.com>

--- luci-app-passwall/luasrc/model/cbi/passwall/client/node_config.lua
+++ luci-app-passwall/luasrc/model/cbi/passwall/client/node_config.lua
@@ -188,6 +188,13 @@ domainStrategy.description = "<br /><ul><li>" .. translate("'AsIs': Only use dom
 domainStrategy:depends("protocol", "_balancing")
 domainStrategy:depends("protocol", "_shunt")
 
+domainMatcher = s:option(ListValue, "domainMatcher", translate("Domain matcher"))
+domainMatcher:value("hybrid")
+domainMatcher:value("linear")
+domainMatcher:depends("protocol", "_balancing")
+domainMatcher:depends("protocol", "_shunt")
+
+
 -- Brook协议
 brook_protocol = s:option(ListValue, "brook_protocol", translate("Protocol"))
 brook_protocol:value("client", translate("Brook"))
@@ -690,6 +697,19 @@ ws_path:depends("ss_transport", "ws")
 ws_path:depends("trojan_transport", "ws")
 ws_path:depends({ type = "Brook", brook_protocol = "wsclient" })
 
+ws_enableEarlyData = s:option(Flag, "ws_enableEarlyData", translate("Enable early data"))
+ws_enableEarlyData:depends("transport", "ws")
+
+ws_maxEarlyData = s:option(Value, "ws_maxEarlyData", translate("Early data length"))
+ws_maxEarlyData.default = "1024"
+ws_maxEarlyData:depends("ws_enableEarlyData", true)
+function ws_maxEarlyData.cfgvalue(self, section)
+	return m:get(section, "ws_maxEarlyData")
+end
+function ws_maxEarlyData.write(self, section, value)
+	m:set(section, "ws_maxEarlyData", value)
+end
+
 -- [[ HTTP/2部分 ]]--
 h2_host = s:option(Value, "h2_host", translate("HTTP/2 Host"))
 h2_host:depends("transport", "h2")
@@ -698,6 +718,17 @@ h2_host:depends("ss_transport", "h2")
 h2_path = s:option(Value, "h2_path", translate("HTTP/2 Path"))
 h2_path:depends("transport", "h2")
 h2_path:depends("ss_transport", "h2")
+
+h2_health_check = s:option(Flag, "h2_health_check", translate("Health check"))
+h2_health_check:depends({ type = "Xray", transport = "h2"})
+
+h2_read_idle_timeout = s:option(Value, "h2_read_idle_timeout", translate("Idle timeout"))
+h2_read_idle_timeout.default = "10"
+h2_read_idle_timeout:depends("h2_health_check", true)
+
+h2_health_check_timeout = s:option(Value, "h2_health_check_timeout", translate("Health check timeout"))
+h2_health_check_timeout.default = "15"
+h2_health_check_timeout:depends("h2_health_check", true)
 
 -- [[ DomainSocket部分 ]]--
 ds_path = s:option(Value, "ds_path", "Path", translate("A legal file path. This file must not exist before running."))
@@ -724,7 +755,22 @@ grpc_serviceName:depends("transport", "grpc")
 grpc_mode = s:option(ListValue, "grpc_mode", "gRPC " .. translate("Transfer mode"))
 grpc_mode:value("gun")
 grpc_mode:value("multi")
-grpc_mode:depends("transport", "grpc")
+grpc_mode:depends({ type = "Xray", transport = "grpc"})
+
+grpc_health_check = s:option(Flag, "grpc_health_check", translate("Health check"))
+grpc_health_check:depends({ type = "Xray", transport = "grpc"})
+
+grpc_idle_timeout = s:option(Value, "grpc_idle_timeout", translate("Idle timeout"))
+grpc_idle_timeout.default = "10"
+grpc_idle_timeout:depends("grpc_health_check", true)
+
+grpc_health_check_timeout = s:option(Value, "grpc_health_check_timeout", translate("Health check timeout"))
+grpc_health_check_timeout.default = "20"
+grpc_health_check_timeout:depends("grpc_health_check", true)
+
+grpc_permit_without_stream = s:option(Flag, "grpc_permit_without_stream", translate("Permit without stream"))
+grpc_permit_without_stream.default = "0"
+grpc_permit_without_stream:depends("grpc_health_check", true)
 
 -- [[ Trojan-Go Shadowsocks2 ]] --
 ss_aead = s:option(Flag, "ss_aead", translate("Shadowsocks secondary encryption"))
@@ -741,11 +787,11 @@ ss_aead_pwd.password = true
 ss_aead_pwd:depends("ss_aead", "1")
 
 -- [[ Trojan-Go Mux ]]--
-mux = s:option(Flag, "smux", translate("smux"))
+mux = s:option(Flag, "smux", translate("Smux"))
 mux:depends("type", "Trojan-Go")
 
 -- [[ Mux ]]--
-mux = s:option(Flag, "mux", translate("mux"))
+mux = s:option(Flag, "mux", translate("Mux"))
 mux:depends({ type = "V2ray", protocol = "vmess" })
 mux:depends({ type = "V2ray", protocol = "vless", xtls = false })
 mux:depends({ type = "V2ray", protocol = "http" })
@@ -759,12 +805,12 @@ mux:depends({ type = "Xray", protocol = "socks" })
 mux:depends({ type = "Xray", protocol = "shadowsocks" })
 mux:depends({ type = "Xray", protocol = "trojan" })
 
-mux_concurrency = s:option(Value, "mux_concurrency", translate("mux concurrency"))
+mux_concurrency = s:option(Value, "mux_concurrency", translate("Mux concurrency"))
 mux_concurrency.default = 8
 mux_concurrency:depends("mux", true)
 mux_concurrency:depends("smux", true)
 
-smux_idle_timeout = s:option(Value, "smux_idle_timeout", translate("mux idle timeout"))
+smux_idle_timeout = s:option(Value, "smux_idle_timeout", translate("Mux idle timeout"))
 smux_idle_timeout.default = 60
 smux_idle_timeout:depends("smux", true)
 
