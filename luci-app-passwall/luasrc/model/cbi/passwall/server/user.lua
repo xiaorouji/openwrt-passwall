f022454fd60c4a3d7297df5727d5871f74310dad
Gzxhwq <gzxhwq@gmail.com>
2022-05-29 13:19:25 +0800

    luci: add support for Xray Shadowsocks-2022

--- luci-app-passwall/luasrc/model/cbi/passwall/server/user.lua
+++ luci-app-passwall/luasrc/model/cbi/passwall/server/user.lua
@@ -33,7 +33,7 @@ local v_ss_encrypt_method_list = {
 }
 
 local x_ss_encrypt_method_list = {
-    "aes-128-gcm", "aes-256-gcm", "chacha20-poly1305", "xchacha20-poly1305"
+    "aes-128-gcm", "aes-256-gcm", "chacha20-poly1305", "xchacha20-poly1305", "2022-blake3-aes-128-gcm", "2022-blake3-aes-256-gcm", "2022-blake3-chacha20-poly1305"
 }
 
 local header_type_list = {
