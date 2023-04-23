a01070d241c1b2e303300b54025cab1979eab2da
xiaorouji <60100640+xiaorouji@users.noreply.github.com>
2023-04-23 16:16:07 +0800

    luci: fix clear ipset/nftset maybe invalid bug

--- luci-app-passwall/luasrc/model/cbi/passwall/client/global.lua
+++ luci-app-passwall/luasrc/model/cbi/passwall/client/global.lua
@@ -400,7 +400,7 @@ end
 o = s:taboption("DNS", Button, "clear_ipset", translate("Clear IPSET"), translate("Try this feature if the rule modification does not take effect."))
 o.inputstyle = "remove"
 function o.write(e, e)
-	luci.sys.call("[ -n \"$(nft list sets 2>/dev/null | grep \"gfwlist\")\" ] && /usr/share/" .. appname .. "/nftables.sh flush_nftset || /usr/share/" .. appname .. "/iptables.sh flush_ipset > /dev/null 2>&1 &")
+	luci.sys.call("[ -n \"$(nft list sets 2>/dev/null | grep \"gfwlist\")\" ] && sh /usr/share/" .. appname .. "/nftables.sh flush_nftset || sh /usr/share/" .. appname .. "/iptables.sh flush_ipset > /dev/null 2>&1 &")
 	luci.http.redirect(api.url("log"))
 end
 
