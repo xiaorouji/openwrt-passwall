e7d9576b6f1c5c355e4e2b589b47f28d7ea774d3
Ariesly <Ariesly.Mao@hotmail.com>
2023-12-12 01:47:22 +0000

    feat: hysteria2 support verify tls pinSHA256

--- luci-app-passwall/luasrc/model/cbi/passwall/client/type/hysteria2.lua
+++ luci-app-passwall/luasrc/model/cbi/passwall/client/type/hysteria2.lua
@@ -44,6 +44,9 @@ o = s:option(Value, option_name("tls_serverName"), translate("Domain"))
 o = s:option(Flag, option_name("tls_allowInsecure"), translate("allowInsecure"), translate("Whether unsafe connections are allowed. When checked, Certificate validation will be skipped."))
 o.default = "0"
 
+o = s:option(Value, option_name("tls_pinSHA256"), translate("PinSHA256"),translate("Certificate fingerprint"))
+o.rewrite_option = o.option
+
 o = s:option(Value, option_name("up_mbps"), translate("Max upload Mbps"))
 o.rewrite_option = o.option
 
