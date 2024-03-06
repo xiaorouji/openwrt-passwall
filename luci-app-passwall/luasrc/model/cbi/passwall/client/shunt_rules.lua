d57f0de40443c1d1432745927c67fccddc2cc23c
Tianhe Y <10704839+nftbty@users.noreply.github.com>
2024-03-06 22:08:04 +0800

    luci: continue to fix #2992

--- luci-app-passwall/luasrc/model/cbi/passwall/client/shunt_rules.lua
+++ luci-app-passwall/luasrc/model/cbi/passwall/client/shunt_rules.lua
@@ -2,7 +2,7 @@ local api = require "luci.passwall.api"
 local appname = api.appname
 local datatypes = api.datatypes
 
-m = Map(appname, "Xray " .. translate("Shunt Rule"))
+m = Map(appname, "Sing-Box/Xray " .. translate("Shunt Rule"))
 m.redirect = api.url()
 api.set_apply_on_parse(m)
 
