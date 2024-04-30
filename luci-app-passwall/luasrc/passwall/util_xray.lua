3f67c72592f64bc0819ee7eac86771a7822dfaef
Tianhe Y <10704839+nftbty@users.noreply.github.com>
2024-05-01 00:20:38 +0800

    luci: fix typo error

--- luci-app-passwall/luasrc/passwall/util_xray.lua
+++ luci-app-passwall/luasrc/passwall/util_xray.lua
@@ -616,7 +616,7 @@ function gen_config(var)
 				settings = {network = "tcp,udp", followRedirect = true},
 				streamSettings = {sockopt = {tproxy = "tproxy"}},
 				sniffing = {
-					enabled = xray_settings.sniffing == "1" or node.protocol == "_shunt",,
+					enabled = xray_settings.sniffing == "1" or node.protocol == "_shunt",
 					destOverride = {"http", "tls", "quic"},
 					metadataOnly = false,
 					routeOnly = (xray_settings.sniffing == "1" and xray_settings.route_only == "1") or (xray_settings.sniffing == "0" and node.protocol == "_shunt"),
