241693dedf9f2949d008d7a4f0e0e875fe6d9476
xiaorouji <60100640+xiaorouji@users.noreply.github.com>
2024-11-19 00:16:28 +0800

    luci: fix Xray Load Balancing Fallback node

--- luci-app-passwall/luasrc/passwall/util_xray.lua
+++ luci-app-passwall/luasrc/passwall/util_xray.lua
@@ -783,13 +783,15 @@ function gen_config(var)
 			if #valid_nodes == 0 then return nil end
 
 			-- fallback node
+			local fallback_node_tag = nil
 			local fallback_node_id = _node.fallback_node
-			if fallback_node_id == "" then fallback_node_id = nil end
+			if fallback_node_id == "" or fallback_node_id == "nil" then fallback_node_id = nil end
 			if fallback_node_id then
 				local is_new_node = true
 				for _, outbound in ipairs(outbounds) do
-					if outbound.tag == fallback_node_id then
+					if outbound.tag:find("^" .. fallback_node_id) == 1 then
 						is_new_node = false
+						fallback_node_tag = outbound.tag
 						break
 					end
 				end
@@ -800,12 +802,7 @@ function gen_config(var)
 						if outbound then
 							outbound.tag = outbound.tag .. ":" .. fallback_node.remarks
 							table.insert(outbounds, outbound)
-						else
-							fallback_node_id = nil
-						end
-					else
-						if not gen_balancer(fallback_node) then
-							fallback_node_id = nil
+							fallback_node_tag = outbound.tag
 						end
 					end
 				end
@@ -813,10 +810,10 @@ function gen_config(var)
 			table.insert(balancers, {
 				tag = balancer_tag,
 				selector = valid_nodes,
-				fallbackTag = fallback_node_id,
+				fallbackTag = fallback_node_tag,
 				strategy = { type = _node.balancingStrategy or "random" }
 			})
-			if _node.balancingStrategy == "leastPing" or fallback_node_id then
+			if _node.balancingStrategy == "leastPing" or fallback_node_tag then
 				if not observatory then
 					observatory = {
 						subjectSelector = { "blc-" },
@@ -1191,13 +1188,8 @@ function gen_config(var)
 
 	--[[
 		local default_dns_flag = "remote"
-		if node_id and tcp_redir_port then
-			local node = uci:get_all(appname, node_id)
-			if node.protocol == "_shunt" then
-				if node.default_node == "_direct" then
-					default_dns_flag = "direct"
-				end
-			end
+		if (not COMMON.default_balancer_tag and not COMMON.default_outbound_tag) or COMMON.default_outbound_tag == "direct" then
+			default_dns_flag = "direct"
 		end
 
 		if dns.servers and #dns.servers > 0 then
