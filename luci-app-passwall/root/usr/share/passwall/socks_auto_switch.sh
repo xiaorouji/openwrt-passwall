f27f8bb572acd5531853ca9c06e3c16fc9391645
A-BO <86697442+lwb1978@users.noreply.github.com>
2025-08-08 23:05:32 +0800

    luci: optimize socks auto switch logic

--- luci-app-passwall/root/usr/share/passwall/socks_auto_switch.sh
+++ luci-app-passwall/root/usr/share/passwall/socks_auto_switch.sh
@@ -31,7 +31,7 @@ test_url() {
 	if /usr/bin/curl --help all | grep -q "\-\-retry-all-errors"; then
 		extra_params="--retry-all-errors ${extra_params}"
 	fi
-	status=$(/usr/bin/curl -I -o /dev/null -skL ${extra_params} --connect-timeout ${timeout} --retry ${try} -w %{http_code} "$url")
+	local status=$(/usr/bin/curl -I -o /dev/null -skL ${extra_params} --connect-timeout ${timeout} --retry ${try} -w %{http_code} "$url")
 	case "$status" in
 		204)
 			status=200
@@ -41,12 +41,12 @@ test_url() {
 }
 
 test_proxy() {
-	result=0
-	status=$(test_url "${probe_url}" ${retry_num} ${connect_timeout} "-x socks5h://127.0.0.1:${socks_port}")
+	local result=0
+	local status=$(test_url "${probe_url}" ${retry_num} ${connect_timeout} "-x socks5h://127.0.0.1:${socks_port}")
 	if [ "$status" = "200" ]; then
 		result=0
 	else
-		status2=$(test_url "https://www.baidu.com" ${retry_num} ${connect_timeout})
+		local status2=$(test_url "https://www.baidu.com" ${retry_num} ${connect_timeout})
 		if [ "$status2" = "200" ]; then
 			result=1
 		else
@@ -68,7 +68,7 @@ test_node() {
 		/usr/share/${CONFIG}/app.sh run_socks flag="test_node_${node_id}" node=${node_id} bind=127.0.0.1 socks_port=${_tmp_port} config_file=test_node_${node_id}.json
 		local curlx="socks5h://127.0.0.1:${_tmp_port}"
 		sleep 1s
-		_proxy_status=$(test_url "${probe_url}" ${retry_num} ${connect_timeout} "-x $curlx")
+		local _proxy_status=$(test_url "${probe_url}" ${retry_num} ${connect_timeout} "-x $curlx")
 		# 结束 SS 插件进程
 		local pid_file="/tmp/etc/${CONFIG}/test_node_${node_id}_plugin.pid"
 		[ -s "$pid_file" ] && kill -9 "$(head -n 1 "$pid_file")" >/dev/null 2>&1
@@ -82,14 +82,14 @@ test_node() {
 }
 
 test_auto_switch() {
-	flag=$(expr $flag + 1)
+	flag=$((flag + 1))
 	local b_nodes=$1
 	local now_node=$2
 	[ -z "$now_node" ] && {
 		if [ -n "$(/usr/share/${CONFIG}/app.sh get_cache_var "socks_${id}")" ]; then
 			now_node=$(/usr/share/${CONFIG}/app.sh get_cache_var "socks_${id}")
 		else
-			#echolog "自动切换检测：未知错误"
+			#echolog "Socks切换检测：未知错误"
 			return 1
 		fi
 	}
@@ -98,58 +98,59 @@ test_auto_switch() {
 		main_node=$now_node
 	}
 
-	status=$(test_proxy)
-	if [ "$status" == 2 ]; then
-		echolog "自动切换检测：无法连接到网络，请检查网络是否正常！"
+	local status=$(test_proxy)
+	if [ "$status" = "2" ]; then
+		echolog "Socks切换检测：无法连接到网络，请检查网络是否正常！"
 		return 2
 	fi
 
 	#检测主节点是否能使用
-	if [ "$restore_switch" == "1" ] && [ -n "$main_node" ] && [ "$now_node" != "$main_node" ]; then
+	if [ "$restore_switch" = "1" ] && [ -n "$main_node" ] && [ "$now_node" != "$main_node" ]; then
 		test_node ${main_node}
 		[ $? -eq 0 ] && {
 			#主节点正常，切换到主节点
-			echolog "自动切换检测：${id}主节点【$(config_n_get $main_node type)：[$(config_n_get $main_node remarks)]】正常，切换到主节点！"
+			echolog "Socks切换检测：${id}主节点【$(config_n_get $main_node type)：[$(config_n_get $main_node remarks)]】正常，切换到主节点！"
 			/usr/share/${CONFIG}/app.sh socks_node_switch flag=${id} new_node=${main_node}
 			[ $? -eq 0 ] && {
-				echolog "自动切换检测：${id}节点切换完毕！"
+				echolog "Socks切换检测：${id}节点切换完毕！"
 			}
 			return 0
 		}
 	fi
 
-	if [ "$status" == 0 ]; then
-		#echolog "自动切换检测：${id}【$(config_n_get $now_node type)：[$(config_n_get $now_node remarks)]】正常。"
+	if [ "$status" = "0" ]; then
+		#echolog "Socks切换检测：${id}【$(config_n_get $now_node type)：[$(config_n_get $now_node remarks)]】正常。"
 		return 0
-	elif [ "$status" == 1 ]; then
-		echolog "自动切换检测：${id}【$(config_n_get $now_node type)：[$(config_n_get $now_node remarks)]】异常，切换到下一个备用节点检测！"
-		local new_node
-		in_backup_nodes=$(echo $b_nodes | grep $now_node)
-		# 判断当前节点是否存在于备用节点列表里
-		if [ -z "$in_backup_nodes" ]; then
-			# 如果不存在，设置第一个节点为新的节点
-			new_node=$(echo $b_nodes | awk -F ' ' '{print $1}')
+	elif [ "$status" = "1" ]; then
+		local new_node msg
+		if [ "$backup_node_num" -gt 1 ]; then
+			# 有多个后备节点时
+			local first_node found node
+			for node in $b_nodes; do
+				[ -z "$first_node" ] && first_node="$node"       # 记录第一个节点
+				[ "$found" = "1" ] && { new_node="$node"; break; } # 找到当前节点后取下一个
+				[ "$node" = "$now_node" ] && found=1             # 标记找到当前节点
+			done
+			# 如果没找到当前节点，或者当前节点是最后一个，就取第一个节点
+			[ -z "$new_node" ] && new_node="$first_node"
+			msg="切换到$([ "$now_node" = "$main_node" ] && echo 备用节点 || echo 下一个备用节点)检测！"
 		else
-			# 如果存在，设置下一个备用节点为新的节点
-			#local count=$(expr $(echo $b_nodes | grep -o ' ' | wc -l) + 1)
-			local next_node=$(echo $b_nodes | awk -F "$now_node" '{print $2}' | awk -F " " '{print $1}')
-			if [ -z "$next_node" ]; then
-				new_node=$(echo $b_nodes | awk -F ' ' '{print $1}')
-			else
-				new_node=$next_node
-			fi
+			# 只有一个后备节点时，与主节点轮询
+			new_node=$([ "$now_node" = "$main_node" ] && echo "$b_nodes" || echo "$main_node")
+			msg="切换到$([ "$now_node" = "$main_node" ] && echo 备用节点 || echo 主节点)检测！"
 		fi
+		echolog "Socks切换检测：${id}【$(config_n_get $now_node type)：[$(config_n_get $now_node remarks)]】异常，$msg"
 		test_node ${new_node}
 		if [ $? -eq 0 ]; then
-			[ "$restore_switch" == "0" ] && {
-				uci set $CONFIG.${id}.node=$new_node
-				[ -z "$(echo $b_nodes | grep $main_node)" ] && uci add_list $CONFIG.${id}.autoswitch_backup_node=$main_node
-				uci commit $CONFIG
-			}
-			echolog "自动切换检测：${id}【$(config_n_get $new_node type)：[$(config_n_get $new_node remarks)]】正常，切换到此节点！"
+#			[ "$restore_switch" = "0" ] && {
+#				uci set $CONFIG.${id}.node=$new_node
+#				[ -z "$(echo $b_nodes | grep $main_node)" ] && uci add_list $CONFIG.${id}.autoswitch_backup_node=$main_node
+#				uci commit $CONFIG
+#			}
+			echolog "Socks切换检测：${id}【$(config_n_get $new_node type)：[$(config_n_get $new_node remarks)]】正常，切换到此节点！"
 			/usr/share/${CONFIG}/app.sh socks_node_switch flag=${id} new_node=${new_node}
 			[ $? -eq 0 ] && {
-				echolog "自动切换检测：${id}节点切换完毕！"
+				echolog "Socks切换检测：${id}节点切换完毕！"
 			}
 			return 0
 		else
@@ -166,12 +167,20 @@ start() {
 	main_node=$(config_n_get $id node)
 	socks_port=$(config_n_get $id port 0)
 	delay=$(config_n_get $id autoswitch_testing_time 30)
-	sleep 5s
 	connect_timeout=$(config_n_get $id autoswitch_connect_timeout 3)
 	retry_num=$(config_n_get $id autoswitch_retry_num 1)
 	restore_switch=$(config_n_get $id autoswitch_restore_switch 0)
 	probe_url=$(config_n_get $id autoswitch_probe_url "https://www.google.com/generate_204")
 	backup_node=$(config_n_get $id autoswitch_backup_node)
+	if [ -n "$backup_node" ]; then
+		backup_node=$(echo "$backup_node" | tr -s ' ' '\n' | uniq | tr -s '\n' ' ')
+		backup_node_num=$(printf "%s\n" "$backup_node" | wc -w)
+		if [ "$backup_node_num" -eq 1 ]; then
+			[ "$main_node" = "$backup_node" ] && return
+		fi
+	else
+		return
+	fi
 	while [ -n "$backup_node" ]; do
 		[ -f "$LOCK_FILE" ] && {
 			sleep 6s
@@ -183,7 +192,6 @@ start() {
 			continue
 		}
 		touch $LOCK_FILE
-		backup_node=$(echo $backup_node | tr -s ' ' '\n' | uniq | tr -s '\n' ' ')
 		test_auto_switch "$backup_node"
 		rm -f $LOCK_FILE
 		sleep ${delay}
@@ -191,4 +199,3 @@ start() {
 }
 
 start $@
-
