402d318edadb3aa0fc62ed68c41a315bfffd089f
A-BO <86697442+lwb1978@users.noreply.github.com>
2025-08-31 20:50:40 +0800

    luci: optimize

--- luci-app-passwall/root/usr/share/passwall/helper_smartdns_add.lua
+++ luci-app-passwall/root/usr/share/passwall/helper_smartdns_add.lua
@@ -214,14 +214,7 @@ if DNS_MODE == "socks" then
 
 		end
 
-		-- 判断是否为本地地址
-		local is_local = w:match("127%.0%.0%.")
-			or w:match("192%.168%.")
-			or w:match("10%.")
-			or w:match("172%.1[6-9]%.")
-			or w:match("172%.2[0-9]%.")
-			or w:match("172%.3[0-1]%.")
-		if not is_local then
+		if not api.is_local_ip(w) then
 			server_param = server_param .. " -proxy " .. proxy_server_name
 		end
 
