5c66de4518be48769099137468818c2e62ca0361
A-BO <86697442+lwb1978@users.noreply.github.com>
2025-11-09 15:24:53 +0800

    Revert "luci: restore save&apply behavior in js luci"
    This reverts commit 071877546abb3e56867ca1cf4389ae0aa3fa44e4.

--- luci-app-passwall/luasrc/model/cbi/passwall/client/node_subscribe.lua
+++ luci-app-passwall/luasrc/model/cbi/passwall/client/node_subscribe.lua
@@ -45,7 +45,6 @@ if has_hysteria2 then
 end
 
 m = Map(appname)
-api.set_apply_on_parse(m)
 
 function m.commit_handler(self)
 	if self.no_commit then
@@ -54,6 +53,22 @@ function m.commit_handler(self)
 	self.uci:foreach(appname, "subscribe_list", function(e)
 		self:del(e[".name"], "md5")
 	end)
+end
+
+if api.is_js_luci() then
+	m.apply_on_parse = false
+	m.on_after_apply = function(self)
+		uci:foreach(appname, "subscribe_list", function(e)
+			uci:delete(appname, e[".name"], "md5")
+		end)
+		uci:commit(appname)
+		api.showMsg_Redirect()
+	end
+end
+
+m.render = function(self, ...)
+	Map.render(self, ...)
+	api.optimize_cbi_ui()
 end
 
 -- [[ Subscribe Settings ]]--
