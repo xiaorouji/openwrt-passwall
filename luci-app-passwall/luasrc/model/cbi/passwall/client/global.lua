6e12d694557193333ef3fa454aa99cc3d26af709
A-BO <86697442+lwb1978@users.noreply.github.com>
2025-11-13 17:24:16 +0800

    luci: fix fakedns configuration error when smartdns is not installed
    fix: https://github.com/xiaorouji/openwrt-passwall/issues/4143

--- luci-app-passwall/luasrc/model/cbi/passwall/client/global.lua
+++ luci-app-passwall/luasrc/model/cbi/passwall/client/global.lua
@@ -529,7 +529,10 @@ o:depends({dns_mode = "xray", dns_shunt = "chinadns-ng"})
 o:depends({smartdns_dns_mode = "xray", dns_shunt = "smartdns"})
 o.validate = function(self, value, t)
 	if value and value == "1" then
-		local _dns_mode = s.fields["dns_mode"]:formvalue(t) or s.fields["smartdns_dns_mode"]:formvalue(t)
+		local _dns_mode = s.fields["dns_mode"]:formvalue(t)
+		if not _dns_mode and s.fields["smartdns_dns_mode"] then
+			_dns_mode = s.fields["smartdns_dns_mode"]:formvalue(t)
+		end
 		local _tcp_node = s.fields["tcp_node"]:formvalue(t)
 		if _dns_mode and _tcp_node then
 			if m:get(_tcp_node, "type"):lower() ~= _dns_mode then
